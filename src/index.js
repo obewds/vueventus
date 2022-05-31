652b61cc13a91b7a6e1f6e412df0256dbea28f59
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-05-30 21:37:48 -0400

    switched vite config to lib mode and added lib index file

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// export { default as ViteVueTsCompoPkgStarter } from './components/ViteVueTsCompoPkgStarter.vue'
+// export { default as extractValidPalettes } from './helpers/extract-valid-palettes'
+// export { default as getBgPaletteColor } from './helpers/get-bg-palette-color'
