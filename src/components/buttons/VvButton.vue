1134411c5fb23fda12e173a451f92a15d7659f2a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-01 03:24:17 -0400

    added new VvButton compo

---
+++ src/components/buttons/VvButton.vue
@@ -0,0 +1,119 @@
+<!--
+    ./src/components/buttons/VvButton.vue
+    @/components/buttons/VvButton.vue
+-->
+
+
+<script setup>
+
+    import { computed, inject } from 'vue'
+    import Buttons from '../../configs/Buttons.js'
+    import Transitions from '../../configs/Transitions.js'
+    import ButtonOutline from '../../configs/palettes/ButtonOutline.js'
+    import ButtonSolid from '../../configs/palettes/ButtonSolid.js'
+
+    const props = defineProps({
+        block: {
+            type: Boolean,
+            default: false,
+        },
+        color: {
+            type: String,
+            default: 'default',
+        },
+        fab: {
+            type: Boolean,
+            default: false,
+        },
+        palette: {
+            type: String,
+            default: 'solid',
+        },
+        size: {
+            type: String,
+            default: 'md',
+        },
+        transitionClasses: {
+            type: String,
+            default: Transitions.classes(),
+        },
+        type: {
+            type: String,
+            default: 'button',
+            validator: (prop) => ['button', 'submit', 'reset'].includes(prop),
+        },
+    })
+
+    const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject('vv') : {
+        buttons: {
+            base: Buttons.base(),
+            palettes: {
+                outline: ButtonOutline,
+                solid: ButtonSolid,
+            },
+            sizes: Buttons.sizes,
+            blockBase: Buttons.blockBase(),
+            blockSizes: Buttons.blockSizes,
+            fabBase: Buttons.fabBase(),
+            fabSizes: Buttons.fabSizes,
+        },
+    }
+
+    let classes = computed( () => {
+
+        let output = []
+
+        if (props.block === true && props.fab === false) {
+
+            if ( vv?.buttons?.blockBase ) {
+                output.push( vv.buttons.blockBase )
+            }
+
+            if ( props.size !== '' && vv?.buttons?.blockSizes?.[props.size] ) {
+                output.push( vv.buttons.blockSizes[props.size] )
+            }
+
+        } else if (props.fab === true && props.block === false) {
+            
+            if ( vv?.buttons?.fabBase ) {
+                output.push( vv.buttons.fabBase )
+            }
+
+            if ( props.size !== '' && vv?.buttons?.fabSizes?.[props.size] ) {
+                output.push( vv.buttons.fabSizes[props.size] )
+            }
+
+        } else {
+            
+            if ( vv?.buttons?.base ) {
+                output.push( vv.buttons.base )
+            }
+
+            if ( props.size !== '' && vv?.buttons?.sizes?.[props.size] ) {
+                output.push( vv.buttons.sizes[props.size] )
+            }
+
+        }  
+
+        if ( props.transitionClasses !== '' ) {
+            output.push( props.transitionClasses )
+        }
+
+        if ( vv?.buttons?.palettes?.[props.palette]?.[props.color] ) {
+            output.push( vv.buttons.palettes[props.palette][props.color] )
+        }
+
+        return output.join(' ')
+
+    })
+
+</script>
+
+
+<template>
+
+    <button :type="type" :class="classes">
+        <slot/>
+    </button>
+
+</template>
