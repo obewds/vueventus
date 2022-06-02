d4991177ae9a4513caa47df5c153fa68a284e218
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-02 12:28:27 -0400

    added dev examples to local pages for Buttons and VvConfig config modules

--- src/views/configs/ButtonsPage.vue
+++ src/views/configs/ButtonsPage.vue
@@ -3,6 +3,7 @@
 <script setup>
 
     import { inject } from 'vue'
+    import VvConfig from '@/configs/VvConfig.js'
     import DevViewArticle from '@/views/compos/DevViewArticle.vue'
 
     const globals = inject('globals')
@@ -14,8 +15,17 @@
 <template>
 
     <DevViewArticle title="Buttons Config Module">
-    
-        Coming Soon!
+
+        <div :class="tw.cardFlex">
+            <div>
+                <h3 :class="tw.h3">Buttons</h3>
+                <div :class="tw.flexWrap">
+                    <div :class="[VvConfig.buttons.classes(), VvConfig.buttons.palettes.solid.primary, tw.pad]">
+                        Buttons.classes()
+                    </div>
+                </div>
+            </div>
+        </div>
     
     </DevViewArticle>
 
