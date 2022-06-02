9cb58b7403cd5373507faa16ff458f20cd3a2422
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-02 11:20:23 -0400

    added socialLinks to viewpress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,20 +9,23 @@ module.exports = {
     },
     themeConfig: {
         logo: 'https://obewds.github.io/vueventus/images/vue-ventus-mark-and-favicon.png',
-        logoLink: 'https://obewds.github.io/vueventus/',
+        logoLink: '/vueventus/',
         nav: [
             { text: "Home", link: "/" },
             { text: "Get Started", link: "/getting-started" },
             { text: "Guides", link: "/guides" },
             { text: "Components", link: "/components" },
-            // { text: "Tests", link: "/tests" },
-            { text: "npm", link: "https://www.npmjs.com/package/@obewds/vueventus" },
-            { text: "GitHub", link: "https://github.com/obewds/vueventus" },
+            //{ text: "npm", link: "https://www.npmjs.com/package/@obewds/vueventus" },
+            //{ text: "GitHub", link: "https://github.com/obewds/vueventus" },
         ],
         footer: {
-            message: 'Released under the MIT License.',
-            copyright: 'Copyright © 2022-present Matt McT LLC'
+            message: 'Released under the MIT License',
+            copyright: 'Copyright © 2022 Matt McT LLC'
         },
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/obewds/vueventus' },
+            { icon: 'twitter', link: 'https://mobile.twitter.com/obewds' },
+        ]
         // sidebarDepth: 2,
         // sidebar: [
             // { text: "Installation", link: "/#installation" },
