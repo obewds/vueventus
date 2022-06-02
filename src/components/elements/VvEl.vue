92f5ad8e086cb5dc9568a1cd31608631773f2aac
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-02 12:27:03 -0400

    removed non VvConfig config module imports and set border default to ''

--- src/components/elements/VvEl.vue
+++ src/components/elements/VvEl.vue
@@ -7,10 +7,7 @@
 <script setup>
 
     import { computed, inject } from 'vue'
-    import Buttons from '../../configs/Buttons.js'
-    import Transitions from '../../configs/Transitions.js'
-    import ButtonOutline from '../../configs/palettes/ButtonOutline.js'
-    import ButtonSolid from '../../configs/palettes/ButtonSolid.js'
+    import VvConfig from '../../configs/VvConfig.js'
 
     const props = defineProps({
         borderPalette: {
@@ -19,7 +16,7 @@
         },
         borderColor: {
             type: String,
-            default: 'default',
+            default: '',
         },
         groundPalette: {
             type: String,
@@ -43,63 +40,24 @@
         },
     })
 
-    const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject('vv') : {
-        buttons: {
-            base: Buttons.base(),
-            palettes: {
-                outline: ButtonOutline,
-                solid: ButtonSolid,
-            },
-            sizes: Buttons.sizes,
-            blockBase: Buttons.blockBase(),
-            blockSizes: Buttons.blockSizes,
-            fabBase: Buttons.fabBase(),
-            fabSizes: Buttons.fabSizes,
-        },
-    }
+    const globals = inject('globals')
+
+    const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject('vv') : VvConfig
 
     let classes = computed( () => {
 
         let output = []
 
-        if (props.block === true && props.fab === false) {
-
-            if ( vv?.buttons?.blockBase ) {
-                output.push( vv.buttons.blockBase )
-            }
-
-            if ( props.size !== '' && vv?.buttons?.blockSizes?.[props.size] ) {
-                output.push( vv.buttons.blockSizes[props.size] )
-            }
-
-        } else if (props.fab === true && props.block === false) {
-            
-            if ( vv?.buttons?.fabBase ) {
-                output.push( vv.buttons.fabBase )
-            }
-
-            if ( props.size !== '' && vv?.buttons?.fabSizes?.[props.size] ) {
-                output.push( vv.buttons.fabSizes[props.size] )
-            }
-
-        } else {
-            
-            if ( vv?.buttons?.base ) {
-                output.push( vv.buttons.base )
-            }
-
-            if ( props.size !== '' && vv?.buttons?.sizes?.[props.size] ) {
-                output.push( vv.buttons.sizes[props.size] )
-            }
-
-        }  
-
-        if ( props.transitionClasses !== '' ) {
-            output.push( props.transitionClasses )
+        if ( vv?.borders?.palettes?.[props.borderPalette]?.[props.borderColor] ) {
+            output.push( vv.borders.palettes[props.borderPalette][props.borderColor] )
         }
 
-        if ( vv?.buttons?.palettes?.[props.palette]?.[props.color] ) {
-            output.push( vv.buttons.palettes[props.palette][props.color] )
+        if ( vv?.grounds?.palettes?.[props.groundPalette]?.[props.groundColor] ) {
+            output.push( vv.grounds.palettes[props.groundPalette][props.groundColor] )
+        }
+
+        if ( vv?.text?.palettes?.[props.textPalette]?.[props.textColor] ) {
+            output.push( vv.text.palettes[props.textPalette][props.textColor] )
         }
 
         return output.join(' ')
