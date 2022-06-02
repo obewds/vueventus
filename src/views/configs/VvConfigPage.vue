1a264d8f8395555da96a38763bae8ff339407164
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-02 11:21:48 -0400

    added new local dev page for VvConfig

---
+++ src/views/configs/VvConfigPage.vue
@@ -0,0 +1,22 @@
+<!-- ./src/views/configs/VvConfigPage.vue -->
+
+<script setup>
+
+    import { inject } from 'vue'
+    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
+
+    const globals = inject('globals')
+    const tw = globals.classes
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="VvConfig Module">
+    
+        Coming Soon!
+    
+    </DevViewArticle>
+
+</template>
