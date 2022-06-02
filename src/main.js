9ffa58ecaec2856d4647f1d87871b16ffb0fd738
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-02 01:25:53 -0400

    added globals.js file for local app DRYness

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, provide } from 'vue'
 import router from '@/router/appRouter.js'
 import App from './App.vue'
+import globals from '@/globals.js'
 import './css/tailwind.css'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.provide('globals', globals)
 
 app.mount('#app')
