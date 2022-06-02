a7e7faa84259f35b7020ee73ef8df2644d4028a5
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-02 11:23:00 -0400

    added test for VvConfig

---
+++ tests/configs/VvConfig.test.js
@@ -0,0 +1,10 @@
+// ./tests/configs/VvConfig.test.js
+
+import { VvConfig } from '../../src/index'
+
+
+test('VvConfig component returns a truthy value', async () => {
+
+    expect(VvConfig).toBeTruthy()
+
+})
