9512a387945a62e0dbedcdb26bbaa83d3ca6d126
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-04 08:16:38 -0400

    generated new docs files with changes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -46,7 +46,7 @@ module.exports = {
                     { text: 'Getting Started', link: '/guides/' },
                     { text: 'Install with Vite.js', link: '/guides/install-vite' },
                     { text: 'Install with Vue CLI', link: '/guides/install-vue-cli' },
-                    { text: 'Install with VILT Stack', link: '/guides/install-vilt' },
+                    { text: 'Install with VILT Stack', link: '/guides/install-vilt-stack' },
                     { text: 'Configuration', link: '/guides/configuration' },
                     { text: 'Test Drive: VvButton', link: '/guides/test-drive-vv-button' },
                 ],
--- docs/guides/index.md
+++ docs/guides/index.md
@@ -20,7 +20,7 @@ But first, you'll need to choose your path for installation:
 
 * [Install with Vite.js](/guides/install-vite)
 * [Install with Vue CLI](/guides/install-vue-cli)
-* [Install with VILT Stack](/guides/install-vilt)
+* [Install with VILT Stack](/guides/install-vilt-stack)
 
 ::: warning 
 VueVentus is not currently written with Typescript. There are some components like VueVentus's `VvEl` and `VvConfig` that were written in Typescript, but some pesky bugs pushed this project back into normal Vue 3 SFCs and the composition API to move forward. ¯\\_(ツ)_/¯
