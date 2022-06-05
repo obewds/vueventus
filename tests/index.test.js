3f844f80ad63c120efaece05df2cf1a65a127485
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-05 16:33:51 -0400

    added test for index page

---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+// ./tests/index.test.js
+
+import { Anchors } from '../src/index'
+import { VvConfig } from '../src/index'
+
+
+test('Text that library imports return a truthy value', async () => {
+
+    expect(Anchors).toBeTruthy()
+
+})
