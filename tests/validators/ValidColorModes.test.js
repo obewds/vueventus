be3926a62078fb97548eafe82d787dfe616e3532
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-06 00:53:46 -0400

    added validator tests

---
+++ tests/validators/ValidColorModes.test.js
@@ -0,0 +1,10 @@
+// ./tests/validators/ValidColorModes.test.js
+
+import ValidColorModes from '../../src/validators/ValidColorModes.js'
+
+
+test('ValidColorModes component returns a truthy value', async () => {
+
+    expect(ValidColorModes).toBeTruthy()
+
+})
