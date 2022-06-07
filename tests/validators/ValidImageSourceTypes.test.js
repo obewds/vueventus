4f8d6cc538ede613a08a90bdfabe636910ee5737
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-07 14:45:01 -0400

    added tests for new validators

---
+++ tests/validators/ValidImageSourceTypes.test.js
@@ -0,0 +1,10 @@
+// ./tests/validators/ValidImageSourceTypes.test.js
+
+import ValidImageSourceTypes from '../../src/validators/ValidImageSourceTypes.js'
+
+
+test('ValidImageSourceTypes component returns a truthy value', async () => {
+
+    expect(ValidImageSourceTypes).toBeTruthy()
+
+})
---
+++ tests/validators/ValidVideoSourceTypes.test.js
@@ -0,0 +1,10 @@
+// ./tests/validators/ValidVideoSourceTypes.test.js
+
+import ValidVideoSourceTypes from '../../src/validators/ValidVideoSourceTypes.js'
+
+
+test('ValidVideoSourceTypes component returns a truthy value', async () => {
+
+    expect(ValidVideoSourceTypes).toBeTruthy()
+
+})
