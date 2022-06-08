fe0bb4470f71323078267796875ee9584f4ad090
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-07 22:05:05 -0400

    added docs favicon files and added code to config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,9 +6,9 @@ module.exports = {
 
     base: '/vueventus/',
     lang: 'en-US',
-    title: 'VueVentus Docs',
+    title: title(),
     titleTemplate: 'VueVentus Modular Components Design System',
-    description: 'A documentation site for the VueVentus component design system',
+    description: description(),
     lastUpdated: true,
 
     markdown: {
@@ -18,6 +18,37 @@ module.exports = {
             md.use(require('markdown-it-include'), resolve(__dirname))
         },
     },
+
+    head: [
+        // ['meta', {property: 'og:description', content: 'description is here...'}],
+        ['meta', {name: 'twitter:title', content: title()}],
+        ['meta', {name: 'og:title', content: title()}],
+        ['meta', {name: 'twitter:description', content: description()}],
+        ['meta', {name: 'og:description', content: description()}],
+        ['meta', {name: 'twitter:card', content: 'summary'}],
+        ['meta', {name: 'og:type', content: 'article'}],
+        ['meta', {name: 'og:site_name', content: title()}],
+        ['meta', {name: 'author', content: '@oberocks'}],
+        ['meta', {name: 'twitter:creator', content: '@vueventus'}],
+        ['link', {rel: 'shortcut icon', href: '/favicon.ico'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '57x57', href: 'images/favicons/apple-icon-57x57.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '60x60', href: 'images/favicons/apple-icon-60x60.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '72x72', href: 'images/favicons/apple-icon-72x72.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '76x76', href: 'images/favicons/apple-icon-76x76.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '114x114', href: 'images/favicons/apple-icon-114x114.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '120x120', href: 'images/favicons/apple-icon-120x120.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '144x144', href: 'images/favicons/apple-icon-144x144.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '152x152', href: 'images/favicons/apple-icon-152x152.png'}],
+        ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: 'images/favicons/apple-icon-180x180.png'}],
+        ['link', {rel: 'icon', type: "image/png", sizes: '192x192', href: 'images/favicons/android-icon-192x192.png'}],
+        ['link', {rel: 'icon', type: "image/png", sizes: '32x32', href: 'images/favicons/favicon-32x32.png'}],
+        ['link', {rel: 'icon', type: "image/png", sizes: '96x96', href: 'images/favicons/favicon-96x96.png'}],
+        ['link', {rel: 'icon', type: "image/png", sizes: '16x16', href: 'images/favicons/favicon-16x16.png'}],
+        ['link', {rel: 'manifest', href: 'images/favicons/manifest.json'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#ffffff'}],
+        ['meta', {name: 'msapplication-TileImage', content: 'images/favicons/ms-icon-144x144.png'}],
+        ['meta', {name: 'theme-color', content: '#ffffff'}],
+    ],
 
     themeConfig: {
 
@@ -58,6 +89,14 @@ module.exports = {
 
 }
 
+
+function title() {
+    return 'VueVentus Docs'
+}
+
+function description() {
+    return 'A documentation site for the VueVentus component design system built for Vue.js and Tailwind CSS'
+}
 
 function mainNav() {
     return [
