811abaec8d928daf3fcb23c1d34799fbadc46069
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-09 00:01:34 -0400

    added new extend.animation/keyframes and tests

---
+++ tests/data/extend.keyframes.test.js
@@ -0,0 +1,10 @@
+// ./tests/data/extend.keyframes.test.js
+
+import keyframes from '../../src/data/extend.keyframes.json'
+
+
+test('keyframes component returns a truthy value', async () => {
+
+    expect(keyframes).toBeTruthy()
+
+})
