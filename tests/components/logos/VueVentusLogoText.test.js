b013d75474277ca29d7b1efb3d001815165cd06d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-10 17:18:43 -0400

    added tests for logos compos

---
+++ tests/components/logos/VueVentusLogoText.test.js
@@ -0,0 +1,14 @@
+
+// ./tests/components/logos/VueVentusLogoText.test.js
+
+
+import { mount } from '@vue/test-utils'
+import { VueVentusLogoText } from '../../../src/index'
+
+
+
+test('VueVentusLogoText.vue component mounts successfully', async () => {
+
+    expect(VueVentusLogoText).toBeTruthy()
+
+})
---
+++ tests/components/logos/VueVentusSpinningMarkPage.test.js
@@ -0,0 +1,14 @@
+
+// ./tests/components/logos/VueVentusSpinningMark.test.js
+
+
+import { mount } from '@vue/test-utils'
+import { VueVentusSpinningMark } from '../../../src/index'
+
+
+
+test('VueVentusSpinningMark.vue component mounts successfully', async () => {
+
+    expect(VueVentusSpinningMark).toBeTruthy()
+
+})
