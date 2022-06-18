857984757a3d56ec57b75d32f7f3568e05d014c4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-18 08:54:29 -0400

    added typescript versions of vue components

--- src/components/buttons/VvButton.vue
+++ src/components/buttons/VvButton.vue
@@ -1,86 +1,86 @@
-<!--
-    ./src/components/buttons/VvButton.vue
-    @/components/buttons/VvButton.vue
--->
+<!-- ./src/components/buttons/VvButton.vue -->
 
+<script lang="ts">
 
-<script setup>
-
-    import { computed, inject } from 'vue'
-    import ValidButtonTypes from '../../validators/ValidButtonTypes.js'
+    import { computed, defineComponent, inject } from 'vue'
+    import ValidButtonTypes from '../../validators/ValidButtonTypes'
     import VvConfig from '../../configs/VvConfig.js'
 
-    const props = defineProps({
-        block: {
-            type: Boolean,
-            default: VvConfig.defaults.VvButton.block,
+    export default defineComponent({
+
+        name: 'VvAnchor',
+
+        props: {
+            block: {
+                type: Boolean,
+                default: VvConfig.defaults.VvButton.block,
+            },
+            color: {
+                type: String,
+                default: VvConfig.defaults.VvButton.color,
+            },
+            fab: {
+                type: Boolean,
+                default: VvConfig.defaults.VvButton.fab,
+            },
+            palette: {
+                type: String,
+                default: VvConfig.defaults.VvButton.palette,
+            },
+            size: {
+                type: String,
+                default: VvConfig.defaults.VvButton.size,
+            },
+            type: {
+                type: String,
+                default: VvConfig.defaults.VvButton.type,
+                validator: (prop: ValidButtonTypes) => (ValidButtonTypes).includes(prop),
+            },
         },
-        color: {
-            type: String,
-            default: VvConfig.defaults.VvButton.color,
-        },
-        fab: {
-            type: Boolean,
-            default: VvConfig.defaults.VvButton.fab,
-        },
-        palette: {
-            type: String,
-            default: VvConfig.defaults.VvButton.palette,
-        },
-        size: {
-            type: String,
-            default: VvConfig.defaults.VvButton.size,
-        },
-        type: {
-            type: String,
-            default: VvConfig.defaults.VvButton.type,
-            validator: (prop) => (ValidButtonTypes).includes(prop),
-        },
-    })
 
-    const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject('vv') : VvConfig
+        setup(props){
 
-    let classes = computed( () => {
+            const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject<typeof VvConfig>('vv') : VvConfig
 
-        let output = []
+            const btnType = props.type as ValidButtonTypes
 
-        if (props.block === true && props.fab === false) {
+            let classes = computed( () => {
 
-            if ( vv?.buttons?.blockBase() ) {
-                output.push( vv.buttons.blockBase() )
-            }
+                let output = []
 
-            if ( props.size !== '' && vv?.buttons?.blockSizes?.[props.size] ) {
-                output.push( vv.buttons.blockSizes[props.size] )
-            }
+                if (props.block === true && props.fab === false) {
+                    if ( vv?.buttons?.blockBase() ) {
+                        output.push( vv.buttons.blockBase() )
+                    }
+                    if ( props.size !== '' && vv?.buttons?.blockSizes?.[props.size] ) {
+                        output.push( vv.buttons.blockSizes[props.size] )
+                    }
+                } else if (props.fab === true && props.block === false) {
+                    if ( vv?.buttons?.fabBase() ) {
+                        output.push( vv.buttons.fabBase() )
+                    }
+                    if ( props.size !== '' && vv?.buttons?.fabSizes?.[props.size] ) {
+                        output.push( vv.buttons.fabSizes[props.size] )
+                    }
+                } else {
+                    if ( vv?.buttons?.base() ) {
+                        output.push( vv.buttons.base() )
+                    }
+                    if ( props.size !== '' && vv?.buttons?.sizes?.[props.size] ) {
+                        output.push( vv.buttons.sizes[props.size] )
+                    }
+                }
+                if ( vv?.buttons?.palettes?.[props.palette as keyof typeof vv.buttons.palettes]?.[props.color] ) {
+                    output.push( vv.buttons.palettes[props.palette as keyof typeof vv.buttons.palettes][props.color] )
+                }
 
-        } else if (props.fab === true && props.block === false) {
-            
-            if ( vv?.buttons?.fabBase() ) {
-                output.push( vv.buttons.fabBase() )
-            }
+                return output.join(' ').trim()
 
-            if ( props.size !== '' && vv?.buttons?.fabSizes?.[props.size] ) {
-                output.push( vv.buttons.fabSizes[props.size] )
-            }
+            })
 
-        } else {
-            
-            if ( vv?.buttons?.base() ) {
-                output.push( vv.buttons.base() )
-            }
-
-            if ( props.size !== '' && vv?.buttons?.sizes?.[props.size] ) {
-                output.push( vv.buttons.sizes[props.size] )
-            }
+            return { btnType, classes }
 
         }
-
-        if ( vv?.buttons?.palettes?.[props.palette]?.[props.color] ) {
-            output.push( vv.buttons.palettes[props.palette][props.color] )
-        }
-
-        return output.join(' ').trim()
 
     })
 
@@ -89,7 +89,7 @@
 
 <template>
 
-    <button :type="type" :class="classes">
+    <button :type="btnType" :class="classes">
         <slot/>
     </button>
 
--- src/components/inputs/VvInput.vue
+++ src/components/inputs/VvInput.vue
@@ -1,51 +1,62 @@
-<!--
-    ./src/components/inputs/VvInput.vue
-    @/components/inputs/VvInput.vue
--->
+<!-- ./src/components/inputs/VvInput.vue -->
 
-<script setup>
+<script lang="ts">
 
-    import { computed, inject } from 'vue'
+    import { computed, defineComponent, inject } from 'vue'
+    import ValidInputTypes from '../../validators/ValidInputTypes'
     import VvConfig from '../../configs/VvConfig.js'
 
-    const props = defineProps({
-        color: {
-            type: String,
-            default: VvConfig.defaults.VvInput.color,
+    export default defineComponent({
+
+        name: 'VvEl',
+
+        props: {
+            color: {
+                type: String,
+                default: VvConfig.defaults.VvInput.color,
+            },
+            palette: {
+                type: String,
+                default: VvConfig.defaults.VvInput.palette,
+            },
+            size: {
+                type: String,
+                default: VvConfig.defaults.VvInput.size,
+            },
+            type: {
+                type: String,
+                default: VvConfig.defaults.VvInput.type,
+                validator: (prop: ValidInputTypes) => (ValidInputTypes).includes(prop),
+            },
         },
-        palette: {
-            type: String,
-            default: VvConfig.defaults.VvInput.palette,
+
+        setup (props) {
+            
+            const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject<typeof VvConfig>('vv') : VvConfig
+
+            let classes = computed( () => {
+
+                let output = []
+
+                if ( vv?.inputs?.base() ) {
+                    output.push( vv.inputs.base() )
+                }
+
+                if ( props.size !== '' && vv?.inputs?.sizes?.[props.size] ) {
+                    output.push( vv.inputs.sizes[props.size] )
+                }
+
+                if ( vv?.inputs?.palettes?.[props.palette as keyof typeof vv.inputs.palettes]?.[props.color] ) {
+                    output.push( vv.inputs.palettes[props.palette as keyof typeof vv.inputs.palettes][props.color] )
+                }
+
+                return output.join(' ').trim()
+
+            })
+
+            return { classes }
+
         },
-        size: {
-            type: String,
-            default: VvConfig.defaults.VvInput.size,
-        },
-        type: {
-            type: String,
-            default: VvConfig.defaults.VvInput.type,
-        },
-    })
-
-    const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject('vv') : VvConfig
-
-    let classes = computed( () => {
-
-        let output = []
-
-        if ( vv?.inputs?.base() ) {
-            output.push( vv.inputs.base() )
-        }
-
-        if ( props.size !== '' && vv?.inputs?.sizes?.[props.size] ) {
-            output.push( vv.inputs.sizes[props.size] )
-        }
-
-        if ( vv?.inputs?.palettes?.[props.palette]?.[props.color] ) {
-            output.push( vv.inputs.palettes[props.palette][props.color] )
-        }
-
-        return output.join(' ').trim()
 
     })
 
