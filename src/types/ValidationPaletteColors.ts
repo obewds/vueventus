3e3faf43f85d09d0c8eb83bf8358e2ab1335631c
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-22 01:50:10 -0400

    removed ValidationPaletteColors.ts

--- src/types/ValidationPaletteColors.ts
+++
@@ -1,7 +0,0 @@
-// ./src/types/ValidationPaletteColors.ts
-
-export type ValidationPaletteColors =  Array<'default' | 'error' | 'success' | string | undefined>
-
-
-// #TODO: Add ValidationPaletteColors to docs
-// #TODO: Add ValidationPaletteColors to index.ts
