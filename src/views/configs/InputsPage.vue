7d96bd0dbb1466d58177223565cc7cbe570f24e1
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-06-22 01:47:04 -0400

    switched inputs config pg to use merged configs in dev app

--- src/views/configs/InputsPage.vue
+++ src/views/configs/InputsPage.vue
@@ -2,11 +2,14 @@
 
 <script setup lang="ts">
 
+    import { inject } from 'vue'
     import globals from '../../globals'
     import DevViewArticle from '@/views/compos/DevViewArticle.vue'
     import VvConfig from '../../configs/VvConfig'
 
     const tw = globals.classes
+
+    const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject<typeof VvConfig>('vv') : VvConfig
 
 </script>
 
@@ -20,19 +23,19 @@
                 <h3 :class="tw.h3">Inputs Colors</h3>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes(), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes()), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="Default Color"
                     />
                 </div>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes(), String(VvConfig.inputs.palettes.validation.error)]"
+                        :class="[String(vv?.inputs.classes()), String(vv?.inputs.palettes.validation.error)]"
                         placeholder="Error Color"
                     />
                 </div>
                 <div :class="tw.flexWrap">
                     <input
-                        :class="[VvConfig.inputs.classes(), String(VvConfig.inputs.palettes.validation.success)]"
+                        :class="[String(vv?.inputs.classes()), String(vv?.inputs.palettes.validation.success)]"
                         placeholder="Success Color"
                     />
                 </div>
@@ -44,37 +47,37 @@
                 <h3 :class="tw.h3">Inputs Sizes</h3>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes('xs'), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes('xs')), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="'xs' Size"
                     />
                 </div>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes('sm'), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes('sm')), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="'sm' Size"
                     />
                 </div>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes(), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes()), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="'md' Size"
                     />
                 </div>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes('lg'), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes('lg')), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="'lg' Size"
                     />
                 </div>
                 <div :class="tw.flexWrap + ' pb-3'">
                     <input
-                        :class="[VvConfig.inputs.classes('xl'), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes('xl')), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="'xl' Size"
                     />
                 </div>
                 <div :class="tw.flexWrap">
                     <input
-                        :class="[VvConfig.inputs.classes('2xl'), String(VvConfig.inputs.palettes.validation.default)]"
+                        :class="[String(vv?.inputs.classes('2xl')), String(vv?.inputs.palettes.validation.default)]"
                         placeholder="'2xl' Size"
                     />
                 </div>
