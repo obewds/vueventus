c65a590748131449b5b1b13b9d6cb081bb8472d9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-06 00:03:13 -0400

    added new cli helper to generate/manage the main vue app file depending on... deps

---
+++ cli/helpers/generateMainFileCode.mjs
@@ -0,0 +1,50 @@
+// ./cli/helpers/generateMainFileCode.mjs
+
+export default function (optsObj) {
+
+    const opts = {
+        path: './src/main.ts',
+        faFree: optsObj && optsObj.faFree ? optsObj.faFree : false,
+        faPro: optsObj && optsObj.faPro ? optsObj.faPro : false,
+        gsap: optsObj && optsObj.gsap ? optsObj.gsap : false,
+    }
+
+    let fontAwesomeLine = ''
+
+    if (opts.faPro === true || opts.faFree === true) {
+        if (opts.faFree === true) {
+            fontAwesomeLine = "import './fontawesome'"
+        }
+        if (opts.faPro === true) {
+            fontAwesomeLine = "import './fontawesomePro'"
+        }
+    }
+
+    let gsapLine = ''
+
+    if (opts.gsap === true) {
+        gsapLine = "import './gsap'"
+    }
+
+const output = `
+// ${opts.path}
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import appVv from './app.vv'
+import './css/tailwind.css'
+${gsapLine}
+${fontAwesomeLine}
+
+
+const app = createApp(App)
+
+app.provide('vv', appVv)
+
+app.mount('#app')
+`
+
+    return output
+    
+}
+
