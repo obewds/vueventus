cc7723643c052989d12a89ec53decf3e3aa77a32
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-05 23:54:36 -0400

    added local dev pgs for input default/underlined palettes

---
+++ src/views/configs/palettes/InputDefaultPage.vue
@@ -0,0 +1,31 @@
+<!-- ./src/views/configs/palettes/InputDefaultPage.vue -->
+
+<script setup lang="ts">
+
+    import globals from '../../../globals'
+    import InputDefault from '../../../configs/palettes/InputDefault'
+    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
+
+    const tw = globals.classes
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="InputDefault Palette Config Module">
+
+        <div :class="tw.cardFlex">
+            <div>
+                <h3 :class="tw.h3">InputDefault</h3>
+                <div :class="tw.flexWrap">
+                    <div v-for="color in globals.data.inputColors" :class="[String(InputDefault[color.key]), tw.pad]">
+                        {{ color.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </DevViewArticle>
+
+</template>
---
+++ src/views/configs/palettes/InputUnderlinedPage.vue
@@ -0,0 +1,31 @@
+<!-- ./src/views/configs/palettes/InputUnderlinedPage.vue -->
+
+<script setup lang="ts">
+
+    import globals from '../../../globals'
+    import InputUnderlined from '../../../configs/palettes/InputUnderlined'
+    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
+
+    const tw = globals.classes
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="InputUnderlined Palette Config Module">
+
+        <div :class="tw.cardFlex">
+            <div>
+                <h3 :class="tw.h3">InputUnderlined</h3>
+                <div :class="tw.flexWrap">
+                    <div v-for="color in globals.data.inputColors" :class="[String(InputUnderlined[color.key]), tw.pad]">
+                        {{ color.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </DevViewArticle>
+
+</template>
