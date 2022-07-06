c498e6cbe513231cd68df0234c4c64d8a08313f4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-05 23:52:32 -0400

    New InputDefault and InputUnderlined palette configs

---
+++ src/configs/palettes/InputUnderlined.ts
@@ -0,0 +1,9 @@
+// ./src/configs/palettes/InputUnderlined.ts
+
+import type { DefaultValidationPalette } from '../../types/DefaultValidationPalette'
+
+export default <DefaultValidationPalette> {
+    'default': 'focus:ring-0 border-x-0 border-t-0 border-b-2 focus:border-b-lightBlue-500 focus:border-x-gray-700 dark:focus:border-b-lightBlue-400 border-b-gray-300 dark:border-b-gray-600 bg-gray-50 focus:bg-white dark:bg-gray-800 dark:focus:bg-gray-700 selection:bg-lightBlue-600 dark:selection:bg-lightBlue-500 selection:text-white',
+    error: 'focus:ring-0 border-x-0 border-t-0 border-b-2 text-pink-700 dark:text-pink-200 focus:border-b-pink-500 focus:border-x-gray-700 dark:focus:border-b-pink-400 border-b-pink-300 dark:border-b-pink-700 bg-gray-50 focus:bg-white dark:bg-gray-800 dark:focus:bg-gray-700 placeholder:text-pink-400 placeholder:dark:text-pink-400 selection:bg-pink-600 dark:selection:bg-pink-500 selection:text-white',
+    success: 'focus:ring-0 border-x-0 border-t-0 border-b-2 text-lime-700 dark:text-lime-200 focus:border-b-lime-500 focus:border-x-gray-700 dark:focus:border-b-lime-400 border-b-lime-300 dark:border-b-lime-700 bg-gray-50 focus:bg-white dark:bg-gray-800 dark:focus:bg-gray-700 placeholder:text-lime-500 placeholder:dark:text-lime-500 selection:bg-lime-600 dark:selection:bg-lime-500 selection:text-white',
+}
