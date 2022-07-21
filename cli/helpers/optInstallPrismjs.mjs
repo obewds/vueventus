7a616c3e553ded8eac58af041e7b0865324478d0
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-21 09:22:13 -0400

    added new cli optInstallPrismjs.mjs helper

---
+++ cli/helpers/optInstallPrismjs.mjs
@@ -0,0 +1,31 @@
+// ./cli/helpers/optInstallPrismjs.mjs
+
+// added thu jul 21
+import fs from 'fs-extra'
+import cwd from './cwd.mjs'
+import run from './run.mjs'
+import vvBrand from './vvBrand.mjs'
+
+export default function (userOptionsObject, stackStubsString, prismDepObject) {
+
+    if ( userOptionsObject.deps.includes( prismDepObject.name ) ) {
+
+        run(prismDepObject.install)
+
+        // add optional Prism.js files if the user also selected them
+        if ( userOptionsObject.files.includes( prismDepObject.files.vvPrism.name ) ) {
+            fs.copySync(
+                stackStubsString + prismDepObject.files.vvPrism.name,
+                cwd + prismDepObject.files.vvPrism.path + prismDepObject.files.vvPrism.name,
+            )
+        }
+
+        console.log(`\nThe ${vvBrand} CLI installed/added the ${prismDepObject.name} dep/files successfully!\n`)
+
+        return prismDepObject.packages
+
+    }
+
+    return []
+    
+}
