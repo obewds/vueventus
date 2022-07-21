7424cd751572af69675ab9950fa43ea94fb0e513
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-21 17:58:57 -0400

    added cli ssg VvPrism.vue file

---
+++ cli/stubs/vite-ssg/VvPrism.vue
@@ -0,0 +1,55 @@
+<!-- ./src/components/vv/elements/VvPrism.vue -->
+
+<script lang="ts">
+
+    import { defineComponent, onMounted, onUpdated } from 'vue'
+    import Prism from 'prismjs'
+
+    export default defineComponent({
+
+        setup (props) {
+
+            onMounted(() => {
+                Prism.manual = true
+                Prism.highlightAll()
+            })
+
+            onUpdated(() => {
+                Prism.manual = true
+                Prism.highlightAll()
+            })
+
+        },
+
+    })
+
+</script>
+
+
+<template>
+
+    <div>
+        <slot/>
+    </div>
+
+</template>
+
+
+<style>
+
+    div.code-toolbar > .toolbar {
+        opacity: 1;
+    }
+    div.code-toolbar > .toolbar > .toolbar-item > button.copy-to-clipboard-button {
+        padding: 6px 10px;
+        color: #000000;
+        background-color: #ffffff;
+        text-transform: uppercase;
+        opacity: 0.5;
+        transition: all ease 300ms;
+    }
+    div.code-toolbar > .toolbar > .toolbar-item > button.copy-to-clipboard-button:hover {
+        opacity: 1;
+    }
+    
+</style>
