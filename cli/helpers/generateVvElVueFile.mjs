ab37155c5730cec42e34f66e9ffd6d8608c52c37
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-23 07:13:32 -0400

    added boiler generat files for vv components set

---
+++ cli/helpers/generateVvElVueFile.mjs
@@ -0,0 +1,15 @@
+// ./cli/helpers/generateVvElVueFile.mjs
+
+// TODO: finish adding code from original file to this one
+// TODO: integrate this module into cli build
+// TODO: delete original stubs/vite-ssg and stubs/vue-ts VvScrollUp.vue files
+
+export default function () {
+
+const output = `
+
+`
+
+return output
+
+}
