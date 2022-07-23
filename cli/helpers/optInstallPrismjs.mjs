9cac410e23a99d6149601c87b277ce22a49eaf1a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-22 23:27:49 -0400

    added new prism-theme-vars to optInstallPrismjs.mjs

--- cli/helpers/optInstallPrismjs.mjs
+++ cli/helpers/optInstallPrismjs.mjs
@@ -12,12 +12,31 @@ export default function (userOptionsObject, stackStubsString, prismDepObject) {
         run(prismDepObject.install)
 
         // add optional Prism.js files if the user also selected them
-        if ( userOptionsObject.files.includes( prismDepObject.files.vvPrism.name ) ) {
-            fs.copySync(
-                stackStubsString + prismDepObject.files.vvPrism.name,
-                cwd + prismDepObject.files.vvPrism.path + prismDepObject.files.vvPrism.name,
-            )
-        }
+
+        const prismFiles = Object.keys(prismDepObject.files)
+
+        prismFiles.forEach( (file) => {
+            if ( userOptionsObject.files.includes( prismDepObject.files[file].name ) ) {
+                fs.copySync(
+                    stackStubsString + prismDepObject.files[file].name,
+                    cwd + prismDepObject.files[file].path + prismDepObject.files[file].name,
+                )
+            }
+        })
+
+        // if ( userOptionsObject.files.includes( prismDepObject.files.vvPrism.name ) ) {
+        //     fs.copySync(
+        //         stackStubsString + prismDepObject.files.vvPrism.name,
+        //         cwd + prismDepObject.files.vvPrism.path + prismDepObject.files.vvPrism.name,
+        //     )
+        // }
+
+        // if ( userOptionsObject.files.includes( prismDepObject.files.prismThemeVarsCss.name ) ) {
+        //     fs.copySync(
+        //         stackStubsString + prismDepObject.files.prismThemeVarsCss.name,
+        //         cwd + prismDepObject.files.prismThemeVarsCss.path + prismDepObject.files.prismThemeVarsCss.name,
+        //     )
+        // }
 
         console.log(`\nThe ${vvBrand} CLI installed/added the ${prismDepObject.name} dep/files successfully!\n`)
 
