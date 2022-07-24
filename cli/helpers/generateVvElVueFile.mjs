a6c34c4bf2a800fe9fecc348987ef596caf462d5
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-24 18:27:41 -0400

    added VvEl code to cli generator method

--- cli/helpers/generateVvElVueFile.mjs
+++ cli/helpers/generateVvElVueFile.mjs
@@ -1,12 +1,74 @@
 // ./cli/helpers/generateVvElVueFile.mjs
 
-// TODO: finish adding code from original file to this one
 // TODO: integrate this module into cli build
 // TODO: delete original stubs/vite-ssg and stubs/vue-ts VvScrollUp.vue files
 
 export default function () {
 
-const output = `
+const output = `<!-- ./src/components/vv/elements/VvEl.vue -->
+
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue'
+    import { VvEl } from '@obewds/vueventus'
+    import appVv from '../../../app.vv'
+
+    export default defineComponent({
+
+        components: { VvEl },
+
+        props: {
+            borderPalette: {
+                type: String,
+                default: appVv.defaults.VvEl.borderPalette,
+            },
+            borderColor: {
+                type: String,
+                default: appVv.defaults.VvEl.borderColor,
+            },
+            groundPalette: {
+                type: String,
+                default: appVv.defaults.VvEl.groundPalette,
+            },
+            groundColor: {
+                type: String,
+                default: appVv.defaults.VvEl.groundColor,
+            },
+            tag: {
+                type: String,
+                default: appVv.defaults.VvEl.tag,
+            },
+            textPalette: {
+                type: String,
+                default: appVv.defaults.VvEl.textPalette,
+            },
+            textColor: {
+                type: String,
+                default: appVv.defaults.VvEl.textColor,
+            },
+        },
+
+    })
+
+</script>
+
+
+<template>
+
+    <VvEl
+        :border-palette="borderPalette"
+        :border-color="borderColor"
+        :ground-palette="groundPalette"
+        :ground-color="groundColor"
+        :tag="tag"
+        :text-palette="textPalette"
+        :text-color="textColor"
+    >
+        <slot/>
+    </VvEl>
+
+</template>
 
 `
 
