1dcb7808d7f833c020374715192b96c1cb4e257e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-24 18:29:03 -0400

    added VvFa code to cli generator method

--- cli/helpers/generateVvFaVueFile.mjs
+++ cli/helpers/generateVvFaVueFile.mjs
@@ -1,12 +1,51 @@
 // ./cli/helpers/generateVvFaVueFile.mjs
 
-// TODO: finish adding code from original file to this one
 // TODO: integrate this module into cli build
 // TODO: delete original stubs/vite-ssg and stubs/vue-ts VvFa.vue files
 
 export default function () {
 
-const output = `
+const output = `<!-- ./src/components/vv/elements/VvFa.vue -->
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue'
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+    import { ValidFontAwesomeFamilies, ValidFontAwesomeSizes } from '@obewds/vueventus'
+
+    export default defineComponent({
+
+        components: { FontAwesomeIcon },
+
+        props: {
+            family: {
+                type: String,
+                default: 'fas',
+                validator: (prop: ValidFontAwesomeFamilies) => (ValidFontAwesomeFamilies).includes(prop),
+            },
+            icon: {
+                type: String,
+                default: 'house',
+            },
+            size: {
+                type: String,
+                default: '1x',
+                validator: (prop: ValidFontAwesomeSizes) => (ValidFontAwesomeSizes).includes(prop),
+            },
+        },
+
+    })
+
+</script>
+
+
+<template>
+
+    <client-only>
+        <FontAwesomeIcon :icon="[family, icon]" :size="size"/>
+    </client-only>
+
+</template>
 
 `
 
