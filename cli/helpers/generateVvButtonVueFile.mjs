a6eca2b7513beb28bfec78aa0e687681fe1ea971
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-24 18:27:18 -0400

    added VvButton code to cli generator method

--- cli/helpers/generateVvButtonVueFile.mjs
+++ cli/helpers/generateVvButtonVueFile.mjs
@@ -1,12 +1,68 @@
 // ./cli/helpers/generateVvButtonVueFile.mjs
 
-// TODO: finish adding code from original file to this one
 // TODO: integrate this module into cli build
 // TODO: delete original stubs/vite-ssg and stubs/vue-ts VvScrollUp.vue files
 
 export default function () {
 
-const output = `
+const output = `<!-- ./src/components/vv/buttons/VvButton.vue -->
+
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue'
+    import { ValidButtonTypes, VvButton } from '@obewds/vueventus'
+    import appVv from '../../../app.vv'
+
+    export default defineComponent({
+
+        components: { VvButton },
+
+        props: {
+            block: {
+                type: Boolean,
+                default: appVv.defaults.VvButton.block,
+            },
+            color: {
+                type: String,
+                default: appVv.defaults.VvButton.color,
+            },
+            fab: {
+                type: Boolean,
+                default: appVv.defaults.VvButton.fab,
+            },
+            palette: {
+                type: String,
+                default: appVv.defaults.VvButton.palette,
+            },
+            size: {
+                type: String,
+                default: appVv.defaults.VvButton.size,
+            },
+            type: {
+                type: String,
+                default: appVv.defaults.VvButton.type,
+                validator: (prop: ValidButtonTypes) => (ValidButtonTypes).includes(prop),
+            },
+        },
+
+    })
+
+</script>
+
+
+<template>
+    <VvButton
+        :block="block"
+        :color="color"
+        :fab="fab"
+        :palette="palette"
+        :size="size"
+        :type="type"
+    >
+        <slot/>
+    </VvButton>
+</template>
 
 `
 
