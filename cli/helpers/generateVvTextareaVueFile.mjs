7c3c37ce0ef118130a9170e19b7b991bf4a94c72
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-24 18:28:19 -0400

    added VvTextarea code to cli generator method

--- cli/helpers/generateVvTextareaVueFile.mjs
+++ cli/helpers/generateVvTextareaVueFile.mjs
@@ -1,12 +1,57 @@
 // ./cli/helpers/generateVvTextareaVueFile.mjs
 
-// TODO: finish adding code from original file to this one
 // TODO: integrate this module into cli build
 // TODO: delete original stubs/vite-ssg and stubs/vue-ts VvScrollUp.vue files
 
 export default function () {
 
-const output = `
+const output = `<!-- ./src/components/vv/textareas/VvTextarea.vue -->
+
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue'
+    import { VvTextarea } from '@obewds/vueventus'
+    import appVv from '../../../app.vv'
+
+    export default defineComponent({
+
+        components: { VvTextarea },
+
+        props: {
+            color: {
+                type: String,
+                default: appVv.defaults.VvTextarea.color,
+            },
+            palette: {
+                type: String,
+                default: appVv.defaults.VvTextarea.palette,
+            },
+            size: {
+                type: String,
+                default: appVv.defaults.VvTextarea.size,
+            },
+            rowSize: {
+                type: String,
+                default: appVv.defaults.VvTextarea.rowSize,
+            },
+        },
+
+    })
+
+</script>
+
+
+<template>
+
+    <VvTextarea
+        :color="color"
+        :palette="palette"
+        :size="size"
+        :row-size="rowSize"
+    />
+
+</template>
 
 `
 
