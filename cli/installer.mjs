981c252e12439ee6cd083ec4217a8e30ea2a85be
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-24 18:29:45 -0400

    added installer file and vv-install bin command to package

---
+++ cli/installer.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import vvBrand from './helpers/vvBrand.mjs'
+
+// show vv-install cli start message
+console.log(`
+
+
+    Welcome to the ${vvBrand} CLI vv-installer!
+
+
+`)
+
+console.log(`The ${vvBrand} CLI vv-installer has completed!`)
