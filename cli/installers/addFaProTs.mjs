3b3053280f299602edb40630847e71e5e8210805
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-26 00:42:21 -0400

    integrated vvFaVueFile into addFaProTs

--- cli/installers/addFaProTs.mjs
+++ cli/installers/addFaProTs.mjs
@@ -2,6 +2,8 @@
 
 import fs from 'fs-extra'
 
+import vvFaVueFile from '../generators/vvFaVueFile.mjs'
+
 import cwd from '../helpers/cwd.mjs'
 import run from '../helpers/run.mjs'
 import vvBrand from '../helpers/vvBrand.mjs'
@@ -17,10 +19,11 @@ export default function (userOptionsObject, stackStubsString, faProDepObject) {
 
     // add optional FontAwesome Free files if the user also selected them
     if ( userOptionsObject.files.includes( faProDepObject.files.vvFa.name ) ) {
-        fs.copySync(
-            stackStubsString + faProDepObject.files.vvFa.name,
-            cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name
-        )
+        // fs.copySync(
+        //     stackStubsString + faProDepObject.files.vvFa.name,
+        //     cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name
+        // )
+        fs.writeFileSync(cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name, vvFaVueFile(), { flag: 'w+' })
     }
 
     console.log(`\nThe ${vvBrand} CLI installed/added the ${faProDepObject.name} dep/files successfully!\n`)
