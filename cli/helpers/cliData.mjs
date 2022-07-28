4a31727b3e3b18286ab4a251797ca4121b18a2a9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-28 09:53:49 -0400

    cli clieanup

--- cli/helpers/cliData.mjs
+++ cli/helpers/cliData.mjs
@@ -214,7 +214,7 @@ let baseVvTsCompos = {
         src: vvListItemVueFile(),
     },
     vvTextareaVue: {
-        name: 'vvTextarea.vue',
+        name: 'VvTextarea.vue',
         checked: true,
         path: '/src/components/vv/textareas/',
         src: vvTextareaVueFile(),
