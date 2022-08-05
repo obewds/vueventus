77f15b4b1092c0a4f2c9ed3217ea110897be7551
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-05 12:08:42 -0400

    adding monthNames.ts helper module

---
+++ src/helpers/monthNames.ts
@@ -0,0 +1,19 @@
+// ./src/helpers/monthNames.ts
+
+// TODO: Add tests for monthNames.ts
+// TODO: Add docs for monthNames.ts
+
+export default {
+    english: {
+        long: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    },
+    french: {
+        long: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        short: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jui', 'Jul', 'Aoû', 'Sep', 'Oct', 'Nov', 'Déc'],
+    },
+    spanish: {
+        long: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        short: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    },
+}
