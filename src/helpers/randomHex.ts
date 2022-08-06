2a0b05561c0b11560e8651e0e61bd2283d589f2a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-05 20:02:07 -0400

    refactored randomHex to now use hexadecimals and less chars

--- src/helpers/randomHex.ts
+++ src/helpers/randomHex.ts
@@ -1,16 +1,14 @@
 // ./src/helpers/randomHex.ts
 
-// TODO: Add tests for randomHex.ts
-// TODO: Add docs for randomHex.ts
-
-import getRandomInt from "./getRandomInt"
+import getRandomInt from './getRandomInt'
+import hexadecimals from './hexadecimals'
 
 export default function (): string {
-    const hexChars = ('0123456789abcdef').split('')
+    const chars = hexadecimals()
     const min = 0
     const max = 15
-    const r = hexChars[getRandomInt(min, max)] + hexChars[getRandomInt(min, max)]
-    const g = hexChars[getRandomInt(min, max)] + hexChars[getRandomInt(min, max)]
-    const b = hexChars[getRandomInt(min, max)] + hexChars[getRandomInt(min, max)]
+    const r = chars[getRandomInt(min, max)] + chars[getRandomInt(min, max)]
+    const g = chars[getRandomInt(min, max)] + chars[getRandomInt(min, max)]
+    const b = chars[getRandomInt(min, max)] + chars[getRandomInt(min, max)]
     return '#' + r + g + b
 }
