a019c8d6912bb4b5ad3fc97943ebc67b84a66543
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-05 20:05:42 -0400

    removed done to-do's and switched order of args for easier dev use

--- src/helpers/padNumber.ts
+++ src/helpers/padNumber.ts
@@ -1,8 +1,5 @@
 // ./src/helpers/padNumber.ts
 
-// TODO: Add tests for padNumber.ts
-// TODO: Add docs for padNumber.ts
-
-export default function (number: number, padCharacter = '0', outputLength = 2): string {
+export default function (number: number, outputLength = 2, padCharacter = '0'): string {
     return number.toString().padStart(outputLength, padCharacter)
 }
