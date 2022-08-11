e617cfd246f055bff55dec6af483b0fa173720c8
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-10 20:33:40 -0400

    added new helpers/timestampToDateTimeAmPm.ts

---
+++ src/helpers/timestampToDateTimeAmPm.ts
@@ -0,0 +1,23 @@
+// ./src/helpers/timestampToDateTimeAmPm.ts
+
+import monthNames from './monthNames'
+import padNumber from './padNumber'
+
+export default function (timestamp: number|string, format: 'long'|'short' = 'short', language: 'english'|'french'|'spanish' = 'english', am: 'am'|'AM' = 'AM', pm: 'pm'|'PM' = 'PM'): string {
+
+    var a = new Date(Number(timestamp) * 1000)
+    var monthNamesObj = monthNames()
+    var months = monthNamesObj[language][format]
+    var year = a.getFullYear()
+    var month = months[a.getMonth()]
+    var date = a.getDate()
+    var hours = a.getHours()
+    var minutes = a.getMinutes()
+    var seconds = a.getSeconds()
+    var amPm = hours >= 12 ? pm : am
+    hours = hours % 12
+    hours = hours === 0 ? 12 : hours
+    
+    return month + ' ' + padNumber(date) + ', ' + year + ' ' + hours + ':' + padNumber(minutes) + ':' + padNumber(seconds) + ' ' + amPm
+
+}
