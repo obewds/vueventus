44a317701a4ce9a5cd281cc30a6edb4ddc070053
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-10 20:33:56 -0400

    added new forceBodyClick.test.js file

---
+++ tests/helpers/forceBodyClick.test.js
@@ -0,0 +1,10 @@
+// ./tests/helpers/forceBodyClick.test.js
+
+import { forceBodyClick } from '../../src/index'
+
+
+test('forceBodyClick helper module returns a truthy value', async () => {
+    
+    expect(forceBodyClick).toBeTruthy()
+
+})
