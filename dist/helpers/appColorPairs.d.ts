32fbe05f80f164acd7c6ee45a99f0f5cc50eabd1
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-11 21:51:52 -0400

    removing es-tinycolor dep and all related files to recover downstream builds

--- dist/helpers/appColorPairs.d.ts
+++
@@ -1,3 +0,0 @@
-import type { AppColors } from '../types/AppColors';
-import type { AppColorPairs } from '../types/AppColorPairs';
-export default function (appColorsJson: AppColors, darkGroundText?: string, lightGroundText?: string): AppColorPairs;
