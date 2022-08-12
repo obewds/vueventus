b9dbb5077327fe01c0e334410157b1cd071654f7
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-12 14:09:27 -0400

    0.45.4 patching appColorPairs helper to force string keys and allow easier/cleaner typing for both data and helper

--- src/types/AppColorPairs.ts
+++ src/types/AppColorPairs.ts
@@ -1,8 +1,7 @@
 // ./src/types/AppColorPairs.ts
 
-import type { AppColor } from './AppColor'
 import type { GroundTextColors } from './GroundTextColors'
 
 export interface AppColorPairs {
-    [key: string|number]: GroundTextColors | AppColorPairs | AppColor
+    [key: string|number]: GroundTextColors | AppColorPairs
 }
