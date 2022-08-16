608eff647da103f6f76145b76e172a2741cb45cd
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-15 20:41:46 -0400

    added new VvRadio props to type interface for compo props

--- src/types/VvRadioProps.ts
+++ src/types/VvRadioProps.ts
@@ -5,6 +5,9 @@ import type { DefaultPalettes } from './DefaultPalettes'
 
 export interface VvRadioProps {
     color: DefaultPaletteColors | string
+    checked: boolean
+    darkRadioHex: string
+    lightRadioHex: string
     palette: DefaultPalettes | string
     size: string
 }
