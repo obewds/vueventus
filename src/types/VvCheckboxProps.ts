faf47d93bffac375fcd1249233d05bbeb7ca092b
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-15 20:41:26 -0400

    added new VvCheckbox props to type interface for compo props

--- src/types/VvCheckboxProps.ts
+++ src/types/VvCheckboxProps.ts
@@ -5,6 +5,9 @@ import type { DefaultPalettes } from './DefaultPalettes'
 
 export interface VvCheckboxProps {
     color: DefaultPaletteColors | string
+    checked: boolean
+    darkCheckHex: string
+    lightCheckHex: string
     palette: DefaultPalettes | string
     size: string
 }
