f79765eb551af3a1a459651fe391e67309ef2aa8
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-22 16:35:52 -0400

    added cliStackNames.mjs cli helper module

---
+++ cli/helpers/cliStackNames.mjs
@@ -0,0 +1,6 @@
+// ./cli/helpers/cliStackNames.mjs
+
+export default {
+    vueTwViteTs: 'Vue 3, Tailwind CSS, Vue Router, Vite & Typescript',
+    vueTwViteSsgMdTs: 'Vite-SSG, Vue 3, Tailwind CSS, Pinia, Markdown, Auto-Loading & Typescript',
+}
