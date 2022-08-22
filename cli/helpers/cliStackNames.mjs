7966f5104e7354b63e6724c30578879afab91eb8
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-22 16:52:45 -0400

    added future stack names in comments

--- cli/helpers/cliStackNames.mjs
+++ cli/helpers/cliStackNames.mjs
@@ -3,4 +3,7 @@
 export default {
     vueTwViteTs: 'Vue 3, Tailwind CSS, Vue Router, Vite & Typescript',
     vueTwViteSsgMdTs: 'Vite-SSG, Vue 3, Tailwind CSS, Pinia, Markdown, Auto-Loading & Typescript',
+    // vueTwViteFirebaseTs: { name: 'Vue 3, Tailwind CSS, Vue Router, Firebase, Vite & Typescript', files: {}, deps: {} },
+    // vueTwNuxtViteTs: { name: 'Vue 3, Tailwind CSS, Nuxt 3 & Typescript', files: {}, deps: {} },
+    // vueTwViteLaravelTs: { name: 'Vue 3, Tailwind CSS, Laravel, Vite & Typescript', files: {}, deps: {} },
 }
