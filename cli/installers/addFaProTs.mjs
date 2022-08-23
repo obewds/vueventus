c72eda03ececf5c00a5d34ad92feb1b87f7f987c
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-23 17:12:49 -0400

    integrated new ssg VvFa file for fa pro installer

--- cli/installers/addFaProTs.mjs
+++ cli/installers/addFaProTs.mjs
@@ -9,18 +9,30 @@ import cwd from '../helpers/cwd.mjs'
 import run from '../helpers/run.mjs'
 import vvBrand from '../helpers/vvBrand.mjs'
 
-export default function (userOptionsObject, faProDepObject) {
+export default function (userOptionsObject, faProDepObject, isSsg = false) {
 
     run(faProDepObject.install)
 
     // add FontAwesome Pro base file if the user also selected it
     if ( userOptionsObject.files.includes( faProDepObject.files.fontAwesomeProTs.name ) ) {
+
         fs.outputFileSync(cwd + faProDepObject.files.fontAwesomeProTs.path + faProDepObject.files.fontAwesomeProTs.name, fontAwesomeProTsFile(), { flag: 'w+' })
+
     }
 
     // add FontAwesome Pro component file if the user also selected it
     if ( userOptionsObject.files.includes( faProDepObject.files.vvFa.name ) ) {
-        fs.outputFileSync(cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name, vvFaVueFile(), { flag: 'w+' })
+
+        if (isSsg === false) {
+
+            fs.outputFileSync(cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name, vvFaVueFile(), { flag: 'w+' })
+
+        } else if (isSsg === true) {
+
+            fs.outputFileSync(cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name, vvFaVueSsgFile(), { flag: 'w+' })
+
+        }
+
     }
 
     console.log(`\nThe ${vvBrand} CLI installed/added the ${faProDepObject.name} dep/files successfully!\n`)
