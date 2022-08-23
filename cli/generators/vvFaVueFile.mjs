fb421956fc51d0f1ecf448925cf2282eaefc3081
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-23 17:03:59 -0400

    integrated new getVvFaCode helper to share code between spa/ssg stacks

--- cli/generators/vvFaVueFile.mjs
+++ cli/generators/vvFaVueFile.mjs
@@ -1,51 +1,9 @@
 // ./cli/generators/vvFaVueFile.mjs
 
+import getVvFaCode from '../helpers/getVvFaCode.mjs'
+
 export default function () {
 
-const output = `<!-- ./src/components/vv/elements/VvFa.vue -->
-
-<script lang="ts">
-
-    import { defineComponent } from 'vue'
-    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-    import { ValidFontAwesomeFamilies, ValidFontAwesomeSizes } from '@obewds/vueventus'
-
-    export default defineComponent({
-
-        components: { FontAwesomeIcon },
-
-        props: {
-            family: {
-                type: String,
-                default: 'fas',
-                validator: (prop: ValidFontAwesomeFamilies) => (ValidFontAwesomeFamilies).includes(prop),
-            },
-            icon: {
-                type: String,
-                default: 'house',
-            },
-            size: {
-                type: String,
-                default: '1x',
-                validator: (prop: ValidFontAwesomeSizes) => (ValidFontAwesomeSizes).includes(prop),
-            },
-        },
-
-    })
-
-</script>
-
-
-<template>
-
-    <client-only>
-        <FontAwesomeIcon :icon="[family, icon]" :size="size"/>
-    </client-only>
-
-</template>
-
-`
-
-return output
+    return getVvFaCode(false)
 
 }
