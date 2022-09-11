09f927747d24133a9720c8015d931b73aed86594
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-11 02:13:12 -0400

    added test generator for vv color mode button tests

---
+++ cli/generators/vvColorModeButtonTestJsFile.mjs
@@ -0,0 +1,98 @@
+// ./cli/generators/vvColorModeButtonTestJsFile.mjs
+
+export default function () {
+
+const output = `// ./tests/components/vv/buttons/VvColorModeButton.test.js
+
+import { mount } from '@vue/test-utils'
+import VvColorModeButton from '../../../../src/components/vv/buttons/VvColorModeButton.vue'
+
+
+test('VvColorModeButton.test.vue component mounts successfully', async () => {
+    
+    expect(VvColorModeButton).toBeTruthy()
+
+})
+
+
+test('VvColorModeButton.vue component returns an expected output when the mode prop = "dark"', async () => {
+
+    const wrapper = mount(VvColorModeButton, {
+        props: {
+            mode: 'dark',
+        },
+    })
+
+    expect(wrapper.html()).toContain('svg')
+    
+})
+
+
+test('VvColorModeButton.vue component returns an expected output when the mode prop = "light"', async () => {
+
+    const wrapper = mount(VvColorModeButton, {
+        props: {
+            mode: 'light',
+        },
+    })
+
+    expect(wrapper.html()).toContain('svg')
+    
+})
+
+
+test('VvColorModeButton.vue toggles a "dark" mode icon to a "light" mode icon and vice versa', async () => {
+
+    const wrapper = mount(VvColorModeButton, {
+        props: {
+            mode: 'dark'
+        }
+    })
+
+    const initSvgPath = wrapper.find('path').html()
+
+    await wrapper.find('button').trigger('click')
+
+    const svgPath = wrapper.find('path').html()
+
+    expect(initSvgPath).not.toBe(svgPath)
+
+    await wrapper.find('button').trigger('click')
+
+    const backToOriginalPath = wrapper.find('path').html()
+
+    expect(initSvgPath).toBe(backToOriginalPath)
+
+})
+
+
+test('VvColorModeButton.vue component can check localStorage for a colorMode property stored during a prior user session', async () => {
+
+    const wrapper = mount(VvColorModeButton, {
+        props: {
+            mode: 'dark',
+        },
+        global: {
+            mocks: {
+                localStorage: {
+                    colorMode: 'light',
+                    setItem (key, item) {
+                        this[key] = item
+                    },
+                    getItem (key) { 
+                        return this[key]
+                    }
+                }
+            },
+        },
+    })
+
+    expect(wrapper.html()).toContain('svg')
+    
+})
+
+`
+
+return output
+
+}
