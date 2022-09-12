cb8120582423ed03244ccd77951894553f203c89
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-12 16:11:29 -0400

    added vvRouterLinkTestJsFile generator

---
+++ cli/generators/vvRouterLinkTestJsFile.mjs
@@ -0,0 +1,24 @@
+// ./cli/generators/vvRouterLinkTestJsFile.mjs
+
+// TODO add this file to vitest build processes
+
+export default function () {
+
+const output = `// ./tests/components/vv/anchors/VvRouterLink.test.js
+
+import { mount } from '@vue/test-utils'
+import VvRouterLink from '../../../../src/components/vv/anchors/VvRouterLink.vue'
+import appVv from '../../../../src/app.vv'
+
+
+test('VvRouterLink.vue component mounts successfully', async () => {
+    
+    expect(VvRouterLink).toBeTruthy()
+
+})
+
+`
+
+return output
+
+}
