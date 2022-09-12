a7c5f5d9b41aab7c51230268424c7d730aad31a5
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-12 18:19:50 -0400

    integrated vv fa test file into pro fa install script

--- cli/installers/addFaProTs.mjs
+++ cli/installers/addFaProTs.mjs
@@ -6,6 +6,8 @@ import fontAwesomeProTsFile from '../generators/fontAwesomeProTsFile.mjs'
 import vvFaVueFile from '../generators/vvFaVueFile.mjs'
 import vvFaVueSsgFile from '../generators/vvFaVueSsgFile.mjs'
 
+import vvFaTestJsFilePro from '../generators/vvFaTestJsFilePro.mjs'
+
 import cwd from '../helpers/cwd.mjs'
 import run from '../helpers/run.mjs'
 import vvBrand from '../helpers/vvBrand.mjs'
@@ -31,6 +33,18 @@ export default function (userOptionsObject, faProDepObject, isSsg = false) {
         } else if (isSsg === true) {
 
             fs.outputFileSync(cwd + faProDepObject.files.vvFa.path + faProDepObject.files.vvFa.name, vvFaVueSsgFile(), { flag: 'w+' })
+
+        }
+
+    }
+
+    // check for the vitest dep
+    if ( userOpts.deps.includes( vitestDepObject.name ) ) {
+
+        // install VvFa.test.js file
+        if ( userOpts.files.includes( faProDepObject.files.vvFaTestJs.name ) ) {
+
+            fs.outputFileSync(cwd + faProDepObject.files.vvFaTestJs.path + faProDepObject.files.vvFaTestJs.name, vvFaTestJsFilePro(), { flag: 'w+' })
 
         }
 
