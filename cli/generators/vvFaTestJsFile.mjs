e200dcd12842b4b13700abf7a48aaaf3ff8078a9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-12 18:21:28 -0400

    replaced code with helper code for free mode and removed completed todo

--- cli/generators/vvFaTestJsFile.mjs
+++ cli/generators/vvFaTestJsFile.mjs
@@ -1,38 +1,9 @@
 // ./cli/generators/vvFaTestJsFile.mjs
 
-// TODO add this file to vitest build processes
+import getVvFaTestJsCode from '../helpers/getVvFaTestJsCode.mjs'
 
 export default function () {
 
-const output = `// ./tests/components/vv/elements/VvFa.test.js
-
-import { mount } from '@vue/test-utils'
-import VvFa from '../../../../src/components/vv/elements/VvFa.vue'
-import '../../../../src/fontAwesomePro'
-
-
-test('VvFa.test.vue component mounts successfully', async () => {
-    
-    expect(VvFa).toBeTruthy()
-
-})
-
-
-test('VvFa.vue component accepts family and icon prop values and returns/renders an svg element', async () => {
-
-    const wrapper = mount(VvFa, {
-        props: {
-            family: 'fas',
-            size: '1x'
-        },
-    })
-
-    expect(wrapper.html()).toBeTruthy()
-
-})
-
-`
-
-return output
+    return getVvFaTestJsCode(false)
 
 }
