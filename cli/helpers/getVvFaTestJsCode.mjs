212c329bea7e90fa66a3e6cc0d43e9ba6980d175
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-12 18:16:29 -0400

    added cli/helpers/getVvFaTestJsCode.mjs

---
+++ cli/helpers/getVvFaTestJsCode.mjs
@@ -0,0 +1,40 @@
+// ./cli/helpers/getVvFaTestJsCode.mjs
+
+export default function (isPro = false) {
+
+let fsSrcImport = `import '../../../../src/fontAwesome'`
+
+if (isPro === true) {
+    fsSrcImport = `import '../../../../src/fontAwesomePro'`
+}
+
+return `// ./tests/components/vv/elements/VvFa.test.js
+
+import { mount } from '@vue/test-utils'
+import VvFa from '../../../../src/components/vv/elements/VvFa.vue'
+${fsSrcImport}
+
+
+test('VvFa.test.vue component mounts successfully', async () => {
+    
+    expect(VvFa).toBeTruthy()
+
+})
+
+
+test('VvFa.vue component accepts family and icon prop values and returns/renders an svg element', async () => {
+
+    const wrapper = mount(VvFa, {
+        props: {
+            family: 'fas',
+            size: '1x'
+        },
+    })
+
+    expect(wrapper.html()).toBeTruthy()
+
+})
+
+`
+
+}
