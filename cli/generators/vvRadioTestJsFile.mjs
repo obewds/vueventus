db52ae726b9440887fb79bace313ca74d3a320b6
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-12 11:49:42 -0400

    added cli vvRadioTestJsFile generator

---
+++ cli/generators/vvRadioTestJsFile.mjs
@@ -0,0 +1,40 @@
+// ./cli/generators/vvRadioTestJsFile.mjs
+
+// TODO add this file to vitest build processes
+
+export default function () {
+
+const output = `// ./tests/components/vv/inputs/VvRadio.test.js
+
+import { mount } from '@vue/test-utils'
+import VvRadio from '../../../../src/components/vv/inputs/VvRadio.vue'
+
+
+test('VvRadio.test.vue component mounts successfully', async () => {
+    
+    expect(VvRadio).toBeTruthy()
+
+})
+
+
+
+test('VvRadio.vue component returns a radio input using the component type prop', async () => {
+
+    const testString = 'test-value'
+
+    const wrapper = mount(VvRadio, {
+        props: {
+            value: testString,
+            checked: true,
+        },
+    })
+
+    expect(wrapper.html()).toContain(testString)
+    
+})
+
+`
+
+return output
+
+}
