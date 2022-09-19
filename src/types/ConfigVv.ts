924c1f5b656afedbe24c47e3784412680332ad53
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-19 13:54:08 -0400

    integrated new ConfigDefaults interface into ConfigVv interface

--- src/types/ConfigVv.ts
+++ src/types/ConfigVv.ts
@@ -12,6 +12,8 @@ import type { ConfigText } from './ConfigText'
 import type { ConfigTextareas } from './ConfigTextareas'
 import type { ConfigTransitions } from './ConfigTransitions'
 
+import type { ConfigDefaults } from './ConfigDefaults'
+
 export interface ConfigVv {
     anchors: ConfigAnchors
     borders: ConfigBorders
@@ -24,5 +26,6 @@ export interface ConfigVv {
     text: ConfigText
     textareas: ConfigTextareas
     transitions: ConfigTransitions
+    defaults: ConfigDefaults
     [key: string | number ]: any
 }
