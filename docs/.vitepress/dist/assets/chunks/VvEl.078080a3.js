ab4b12c0816a9c6d1e797ca35db50d111f623af8
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-09-24 12:24:43 -0400

    generated fresh docs files for patch 0.51.2

---
+++ docs/.vitepress/dist/assets/chunks/VvEl.078080a3.js
@@ -0,0 +1,1 @@
+import{b as a}from"./VvTextarea.vue_vue_type_script_lang.eca18dbe.js";import{_ as e,o,m as r,w as t,r as n,n as l,q as c}from"../app.2ed891de.js";function p(s,m,f,i,_,d){return o(),r(c(s.tag),{class:l(s.classes)},{default:t(()=>[n(s.$slots,"default")]),_:3},8,["class"])}const C=e(a,[["render",p]]);export{C as V};
