fd22e96c182c903185775578ef4c5c7641cc546d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-11-28 14:35:13 -0500

    updating CLI b test file generator script so output file coverage is 100%

--- cli/generators/bTestJsFile.mjs
+++ cli/generators/bTestJsFile.mjs
@@ -5,13 +5,35 @@ export default function () {
 const output = `// ./tests/pages/b.test.js
 
 import { mount } from '@vue/test-utils'
+import { createHead } from '@vueuse/head'
+import { createPinia } from 'pinia'
 import b from '../../src/pages/b.vue'
+
+
+const head = createHead()
+const pinia = createPinia()
 
 
 test('b.vue page mounts successfully', async () => {
     
     expect(b).toBeTruthy()
 
+})
+
+
+test('b.vue page component renders the setup() output as expected', async () => {
+
+    const wrapper = mount(b, {
+        global: {
+            plugins: [head, pinia],
+        },
+    })
+
+    expect(wrapper.html()).toContain('<p>Hello I am B</p>')
+    expect(wrapper.html()).toContain('<img src="')
+    expect(wrapper.html()).toContain('" alt="">')
+    expect(wrapper.html()).toContain('Prefetch Result:')
+    
 })
 
 `
