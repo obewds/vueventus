c9b9ba7974e5b1c9afed14c7bcce0d53263acd1a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-11 06:36:12 -0500

    updating GroundMonochromaticPage.vue to page design using just VvConfig data displayed in markup

--- src/views/configs/palettes/GroundMonochromaticPage.vue
+++ src/views/configs/palettes/GroundMonochromaticPage.vue
@@ -2,11 +2,9 @@
 
 <script setup lang="ts">
 
-    import globals from '../../../globals'
-    import GroundMonochromatic from '../../../configs/palettes/GroundMonochromatic'
-    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
-
-    const tw = globals.classes
+    import DevConfigPalettePre from '../../compos/DevConfigPalettePre.vue'
+    import DevViewArticle from '../../compos/DevViewArticle.vue'
+    import VvConfig from '../../../configs/VvConfig'
 
 </script>
 
@@ -15,16 +13,7 @@
 
     <DevViewArticle title="GroundMonochromatic Palette Config Module">
 
-        <div :class="tw.cardFlex">
-            <div>
-                <h3 :class="tw.h3">GroundMonochromatic</h3>
-                <div :class="tw.flexWrap">
-                    <div v-for="color in globals.data.colors" :class="[String(GroundMonochromatic[color.key]), tw.pad]">
-                        {{ color.name }}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <DevConfigPalettePre :data="VvConfig.grounds.palettes.monochromatic"/>
     
     </DevViewArticle>
 
