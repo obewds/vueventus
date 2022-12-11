f8f501b34fcca6943da95e0a14942a29e37b450d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-11 06:37:02 -0500

    updating InputUnderlinedPage.vue to page design using just VvConfig data displayed in markup

--- src/views/configs/palettes/InputUnderlinedPage.vue
+++ src/views/configs/palettes/InputUnderlinedPage.vue
@@ -2,11 +2,9 @@
 
 <script setup lang="ts">
 
-    import globals from '../../../globals'
-    import InputUnderlined from '../../../configs/palettes/InputUnderlined'
-    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
-
-    const tw = globals.classes
+    import DevConfigPalettePre from '../../compos/DevConfigPalettePre.vue'
+    import DevViewArticle from '../../compos/DevViewArticle.vue'
+    import VvConfig from '../../../configs/VvConfig'
 
 </script>
 
@@ -15,16 +13,7 @@
 
     <DevViewArticle title="InputUnderlined Palette Config Module">
 
-        <div :class="tw.cardFlex">
-            <div>
-                <h3 :class="tw.h3">InputUnderlined</h3>
-                <div :class="tw.flexWrap">
-                    <div v-for="color in globals.data.inputColors" :class="[String(InputUnderlined[color.key]), tw.pad]">
-                        {{ color.name }}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <DevConfigPalettePre :data="VvConfig.inputs.palettes.underlined"/>
     
     </DevViewArticle>
 
