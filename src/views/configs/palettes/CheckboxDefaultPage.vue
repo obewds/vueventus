cd604f8176e73a165249fb94cddc6582af7843f3
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-11 06:32:45 -0500

    updating CheckboxDefaultPage.vue to page design using just VvConfig data displayed in markup

--- src/views/configs/palettes/CheckboxDefaultPage.vue
+++ src/views/configs/palettes/CheckboxDefaultPage.vue
@@ -2,12 +2,9 @@
 
 <script setup lang="ts">
 
-    import globals from '../../../globals'
-    import CheckboxDefault from '../../../configs/palettes/CheckboxDefault'
-    import VvCheckbox from '../../../components/inputs/VvCheckbox.vue'
-    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
-
-    const tw = globals.classes
+    import DevConfigPalettePre from '../../compos/DevConfigPalettePre.vue'
+    import DevViewArticle from '../../compos/DevViewArticle.vue'
+    import VvConfig from '../../../configs/VvConfig'
 
 </script>
 
@@ -16,18 +13,7 @@
 
     <DevViewArticle title="CheckboxDefault Palette Config Module">
 
-        <div :class="tw.cardFlex">
-            <div>
-                <h3 :class="tw.h3">CheckboxDefault</h3>
-                <div :class="tw.flexWrap">
-                    <VvCheckbox
-                        v-for="color in globals.data.colors"
-                        :color="color.key"
-                        checked
-                    />
-                </div>
-            </div>
-        </div>
+        <DevConfigPalettePre :data="VvConfig.checkboxes.palettes.default"/>
     
     </DevViewArticle>
 
