22ca461f89be1a75bb4a12c6414de4ebaa3f98a6
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-11 06:36:43 -0500

    updating GroundPastelPage.vue to page design using just VvConfig data displayed in markup

--- src/views/configs/palettes/GroundPastelPage.vue
+++ src/views/configs/palettes/GroundPastelPage.vue
@@ -2,11 +2,9 @@
 
 <script setup lang="ts">
 
-    import globals from '../../../globals'
-    import GroundPastel from '../../../configs/palettes/GroundPastel'
-    import DevViewArticle from '@/views/compos/DevViewArticle.vue'
-
-    const tw = globals.classes
+    import DevConfigPalettePre from '../../compos/DevConfigPalettePre.vue'
+    import DevViewArticle from '../../compos/DevViewArticle.vue'
+    import VvConfig from '../../../configs/VvConfig'
 
 </script>
 
@@ -15,16 +13,7 @@
 
     <DevViewArticle title="GroundPastel Palette Config Module">
 
-        <div :class="tw.cardFlex">
-            <div>
-                <h3 :class="tw.h3">GroundPastel</h3>
-                <div :class="tw.flexWrap">
-                    <div v-for="color in globals.data.colors" :class="[String(GroundPastel[color.key]), tw.pad]">
-                        {{ color.name }}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <DevConfigPalettePre :data="VvConfig.grounds.palettes.pastel"/>
     
     </DevViewArticle>
 
