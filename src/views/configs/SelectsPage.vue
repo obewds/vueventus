2b2355d200b0a852b038930e6a720853309ae4b9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:36:22 -0500

    adding new SelectsPage.vue to local dev app compos

---
+++ src/views/configs/SelectsPage.vue
@@ -0,0 +1,20 @@
+<!-- ./src/views/configs/SelectsPage.vue -->
+
+<script setup lang="ts">
+
+    import DevConfigPres from '../compos/DevConfigPres.vue'
+    import DevViewArticle from '../compos/DevViewArticle.vue'
+    import VvConfig from '../../configs/VvConfig'
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="Selects Config Module">
+
+        <DevConfigPres :data="VvConfig.selects"/>
+    
+    </DevViewArticle>
+
+</template>
