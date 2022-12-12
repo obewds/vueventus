aa347c5c389d31e70780a684d95bd4261aef0dfe
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:38:55 -0500

    adding new vvSelectVueFile.mjs cli script

---
+++ cli/generators/vvSelectVueFile.mjs
@@ -0,0 +1,52 @@
+// ./cli/generators/vvSelectVueFile.mjs
+
+export default function () {
+
+const output = `<!-- ./src/components/vv/selects/VvSelect.vue -->
+
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue'
+    import { VvSelect } from '@obewds/vueventus'
+    import appVv from '../../../app.vv'
+
+    export default defineComponent({
+
+        components: { VvSelect },
+
+        props: {
+            color: {
+                type: String,
+                default: appVv.defaults.VvSelect.color,
+            },
+            palette: {
+                type: String,
+                default: appVv.defaults.VvSelect.palette,
+            },
+            size: {
+                type: String,
+                default: appVv.defaults.VvSelect.size,
+            },
+        },
+
+    })
+
+</script>
+
+
+<template>
+
+    <VvSelect
+        :color="color"
+        :palette="palette"
+        :size="size"
+    />
+
+</template>
+
+`
+
+return output
+
+}
