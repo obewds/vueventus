f22cba55831446f6851237b4227518ae958c27d8
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:01:24 -0500

    adding new tests/components/elements/VvListbox.test.js w/ full coverage

---
+++ tests/components/selects/VvListbox.test.js
@@ -0,0 +1,74 @@
+// ./tests/components/selects/VvListbox.test.js
+
+
+import { mount } from '@vue/test-utils'
+import { VvListbox } from '../../../src/index'
+import { VvConfig } from '../../../src/index'
+
+
+
+test('VvListbox.vue component mounts successfully', async () => {
+
+    expect(VvListbox).toBeTruthy()
+
+})
+
+
+
+test('VvListbox.vue component accepts an object literal slot value with markup and text content', async () => {
+
+    const testString = `<div>Fake Headless UI Clickable Label</div>`
+
+    const wrapper = mount(VvListbox, {
+        slots: {
+            default: testString,
+        },
+    })
+
+    expect(wrapper.html()).toContain(testString)
+    
+})
+
+
+
+test('VvListbox.vue component can successfully inject() a parent component provide() property "vv"', async () => {
+
+    const wrapper = mount(VvListbox, {
+        global: {
+            provide: {
+                vv: VvConfig,
+            },
+        },
+    })
+
+    expect(wrapper.html()).toContain('<button')
+    expect(wrapper.html()).toContain('</button')
+    
+})
+
+
+
+test('VvListbox.vue component accepts basic props values and returns expected markup', async () => {
+
+    const wrapper = mount(VvListbox, {
+        props: {
+            label: 'Testing Label:',
+            listboxButtonColor: 'error',
+            listboxOptionColor: 'error',
+        },
+    })
+
+    expect(wrapper.html()).toContain('<button')
+    expect(wrapper.html()).toContain('</button')
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.html()).toContain('<li')
+    expect(wrapper.html()).toContain('</li')
+
+    await wrapper.find('li').trigger('click')
+
+    expect(wrapper.html()).not.toContain('<li')
+    expect(wrapper.html()).not.toContain('</li')
+    
+})
