2a44db016ef28ed955ef9e47f8e94fdbfbab3d65
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 11:16:50 -0500

    updating VvListProps ts syntax to interface

--- src/types/VvListProps.ts
+++ src/types/VvListProps.ts
@@ -6,6 +6,7 @@ import type { DefaultPaletteColors } from './DefaultPaletteColors'
 import type { DefaultPalettes } from './DefaultPalettes'
 
 export interface VvListProps {
+    listStyleTypeClass: string
     markerPalette: DefaultPalettes | string
     markerColor: DefaultPaletteColors | string
     tag: ValidListTypes
