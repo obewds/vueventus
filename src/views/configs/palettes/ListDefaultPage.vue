0ad510bb0aebde27e46156cbd76a795e9b665504
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:36:03 -0500

    adding new ListDefault.vue to local dev app compos

---
+++ src/views/configs/palettes/ListDefaultPage.vue
@@ -0,0 +1,20 @@
+<!-- ./src/views/configs/palettes/ListDefault.vue -->
+
+<script setup lang="ts">
+
+    import DevConfigPalettePre from '../../compos/DevConfigPalettePre.vue'
+    import DevViewArticle from '../../compos/DevViewArticle.vue'
+    import VvConfig from '../../../configs/VvConfig'
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="ListDefault Palette Config Module">
+
+        <DevConfigPalettePre :data="VvConfig.lists.palettes.default"/>
+    
+    </DevViewArticle>
+
+</template>
