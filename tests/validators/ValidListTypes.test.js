7e63d521cd7a8bd792d914e2ec8c2c25bf63536e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:03:38 -0500

    adding new tests/components/elements/ValidListTypes.test.js w/ full coverage

---
+++ tests/validators/ValidListTypes.test.js
@@ -0,0 +1,10 @@
+// ./tests/validators/ValidListTypes.test.js
+
+import { ValidListTypes } from '../../src/index'
+
+
+test('ValidListTypes component returns a truthy value', async () => {
+
+    expect(ValidListTypes).toBeTruthy()
+
+})
