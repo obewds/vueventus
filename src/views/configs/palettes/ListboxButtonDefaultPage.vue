784f97c96f4e55d30d1c779c846d7c3b3653305f
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:31:37 -0500

    adding new ListboxButtonDefaultPage.vue to local dev app compos

---
+++ src/views/configs/palettes/ListboxButtonDefaultPage.vue
@@ -0,0 +1,20 @@
+<!-- ./src/views/configs/palettes/ListboxButtonDefaultPage.vue -->
+
+<script setup lang="ts">
+
+    import DevConfigPalettePre from '../../compos/DevConfigPalettePre.vue'
+    import DevViewArticle from '../../compos/DevViewArticle.vue'
+    import VvConfig from '../../../configs/VvConfig'
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="ListboxButtonDefault Palette Config Module">
+
+        <DevConfigPalettePre :data="VvConfig.listboxes.buttonPalettes.default"/>
+    
+    </DevViewArticle>
+
+</template>
