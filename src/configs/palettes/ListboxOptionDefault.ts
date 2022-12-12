8ab337d4247bc4762b1b2032952b76aac167ff1e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:10:40 -0500

    adding new ListboxOptionDefault palette module

---
+++ src/configs/palettes/ListboxOptionDefault.ts
@@ -0,0 +1,9 @@
+// ./src/configs/palettes/ListboxOptionDefault.ts
+
+import type { DefaultValidationPalette } from '../../types/DefaultValidationPalette'
+
+export default <DefaultValidationPalette> {
+    'default': 'ui-active:font-bold ui-active:bg-blue-500 ui-active:text-white ui-not-active:bg-white ui-not-active:text-gray-900 dark:ui-not-active:bg-gray-700 dark:ui-not-active:text-white',
+    error: 'ui-active:font-bold ui-active:bg-rose-500 ui-active:text-white ui-not-active:bg-white ui-not-active:text-gray-900 dark:ui-not-active:bg-gray-700 dark:ui-not-active:text-white',
+    success: 'ui-active:font-bold ui-active:bg-green-500 ui-active:text-white ui-not-active:bg-white ui-not-active:text-gray-900 dark:ui-not-active:bg-gray-700 dark:ui-not-active:text-white',
+}
