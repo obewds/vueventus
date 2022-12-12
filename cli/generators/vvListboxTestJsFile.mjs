c0e81f9cfc5d718b11fca07f9be0294bb9d743a1
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:38:01 -0500

    adding new vvListboxTestJsFile.mjs cli script

---
+++ cli/generators/vvListboxTestJsFile.mjs
@@ -0,0 +1,21 @@
+// ./cli/generators/vvListboxTestJsFile.mjs
+
+export default function () {
+
+const output = `// ./tests/components/vv/selects/VvListbox.test.js
+
+import { mount } from '@vue/test-utils'
+import VvListbox from '../../../../src/components/vv/selects/VvListbox.vue'
+
+
+test('VvListbox.test.vue component mounts successfully', async () => {
+    
+    expect(VvListbox).toBeTruthy()
+
+})
+
+`
+
+return output
+
+}
