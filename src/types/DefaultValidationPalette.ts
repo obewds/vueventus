5ffb7322ea9fd3952ee9b93efa599cd63e740141
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 11:16:32 -0500

    updating DefaultValidationPalette ts syntax to interface

--- src/types/DefaultValidationPalette.ts
+++ src/types/DefaultValidationPalette.ts
@@ -1,10 +1,7 @@
 // ./src/types/DefaultValidationPalette.ts
 
-// TODO: change this to an interface syntax like ./src/types/DefaultValidationPalette.ts ???
-
 export interface DefaultValidationPalette {
     default?: string
-    error?: string
-    success?: string
+    underlined?: string
     [key: string]: string | undefined
 }
