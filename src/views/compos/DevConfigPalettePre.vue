e2beabb5ed070772bdef33d7e544e84bfe87dbe6
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:30:53 -0500

    adding new compos/DevConfigPalettePre.vue compo for local dev testing app

---
+++ src/views/compos/DevConfigPalettePre.vue
@@ -0,0 +1,33 @@
+<!-- ./src/views/compos/DevConfigPalettePre.vue -->
+
+<script setup lang="ts">
+
+    import globals from '../../globals'
+    import VvEl from '../../components/elements/VvEl.vue'
+
+    const props = defineProps({
+        data: {
+            type: Object,
+            required: true,
+        },
+    })
+
+    const tw = globals.classes
+
+</script>
+
+
+<template>
+
+    <div>
+
+        <VvEl tag="pre" ground-palette="console" ground-color="success" :class="tw.configPre">
+
+            <code>{{ data }}</code>
+
+        </VvEl>
+
+    </div>
+    
+
+</template>
