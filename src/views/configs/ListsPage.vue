a3eff6de29b967923f90a46180e04aa1d81cb8f5
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-12 10:35:40 -0500

    adding new ListsPage.vue to local dev app compos

---
+++ src/views/configs/ListsPage.vue
@@ -0,0 +1,20 @@
+<!-- ./src/views/configs/ListsPage.vue -->
+
+<script setup lang="ts">
+
+    import DevConfigPres from '../compos/DevConfigPres.vue'
+    import DevViewArticle from '../compos/DevViewArticle.vue'
+    import VvConfig from '../../configs/VvConfig'
+
+</script>
+
+
+<template>
+
+    <DevViewArticle title="Lists Config Module">
+
+        <DevConfigPres :data="VvConfig.lists"/>
+    
+    </DevViewArticle>
+
+</template>
