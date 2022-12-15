3dd5415a561c2caf6db5055688abe372cd1bd37c
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-15 11:37:49 -0500

    adding new VvList sizes examples to test with in local dev app for pkg

--- src/views/components/lists/VvListPage.vue
+++ src/views/components/lists/VvListPage.vue
@@ -23,17 +23,30 @@
         <div :class="tw.cardFlex">
             <div>
                 <h3 :class="tw.h3">VvList Default Palette</h3>
-                <ul class="list-disc text-left pl-4">
-                    <VvList
-                        v-for="color in globals.data.colors"
-                        tag="ul"
-                        :marker-color="color.key"
-                    >
-                        <li>{{ color.name }} Marker Color Example One</li>
-                        <li>{{ color.name }} Example Two</li>
-                        <li>{{ color.name }} Marker Example</li>
-                    </VvList>
-                </ul>
+                <VvList
+                    v-for="color in globals.data.colors"
+                    tag="ul"
+                    :marker-color="color.key"
+                >
+                    <li>{{ color.name }} Marker Color Example One</li>
+                    <li>{{ color.name }} Example Two</li>
+                    <li>{{ color.name }} Marker Example</li>
+                </VvList>
+            </div>
+        </div>
+
+        <div :class="tw.cardFlex">
+            <div>
+                <h3 :class="tw.h3">VvList Sizes</h3>
+                <VvList
+                    v-for="size in globals.data.textSizes"
+                    :size="size.name"
+                    class="pb-6"
+                >
+                    <li>{{ size.name }} Example One</li>
+                    <li>{{ size.name }} Example Two</li>
+                    <li>{{ size.name }} Example Three</li>
+                </VvList>
             </div>
         </div>
 
