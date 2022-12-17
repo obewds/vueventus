706d7dc99ee9ad28880a230028b5600f0050b204
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-16 22:23:21 -0500

    adding new SizesInputs interface

---
+++ src/types/SizesInputs.ts
@@ -0,0 +1,11 @@
+// ./src/types/SizesInputs.ts
+
+export interface SizesInputs {
+    'xs'?: string | undefined
+    'sm'?: string | undefined
+    'md'?: string | undefined
+    'lg'?: string | undefined
+    'xl'?: string | undefined
+    '2xl'?: string | undefined
+    [key: string]: string | undefined
+}
