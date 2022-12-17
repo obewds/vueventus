0faaac5c2686abba38acc1730dc622a78cc247c9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-16 22:49:29 -0500

    updating VvAnchorProps with new interfaces and changes

--- src/types/VvAnchorProps.ts
+++ src/types/VvAnchorProps.ts
@@ -2,15 +2,17 @@
 
 import type { DefaultPaletteColors } from './DefaultPaletteColors'
 import type { DefaultPalettes } from './DefaultPalettes'
+import type { SizesButtons } from './SizesButtons'
+import type { SizesText } from './SizesText'
 
 export interface VvAnchorProps {
     button: boolean
     buttonBlock: boolean
     buttonFab: boolean
-    buttonSize: string
-    color: DefaultPaletteColors | string
+    buttonSize: keyof SizesButtons | string
+    color: keyof DefaultPaletteColors | string
     external: boolean
     href: string
-    palette: DefaultPalettes | string
-    textSize: string
+    palette: keyof DefaultPalettes | string
+    textSize: keyof SizesText | string
 }
