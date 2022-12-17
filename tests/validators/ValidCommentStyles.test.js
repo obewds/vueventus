3dbb38cf5c3b0d9ce42536dc31c4d7d09ddb6c42
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-16 21:56:19 -0500

    adding new tests/validators/ValidCommentStyles.test.js

---
+++ tests/validators/ValidCommentStyles.test.js
@@ -0,0 +1,10 @@
+// ./tests/validators/ValidCommentStyles.test.js
+
+import { ValidCommentStyles } from '../../src/index'
+
+
+test('ValidCommentStyles component returns a truthy value', async () => {
+
+    expect(ValidCommentStyles).toBeTruthy()
+
+})
