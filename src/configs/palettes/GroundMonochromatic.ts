8e459c91be375fd594dc09df3d7504d9563c5b96
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-16 22:13:15 -0500

    updating GroundMonochromatic module to now use DefaultPaletteColors interface

--- src/configs/palettes/GroundMonochromatic.ts
+++ src/configs/palettes/GroundMonochromatic.ts
@@ -1,8 +1,8 @@
 // ./src/configs/palettes/GroundMonochromatic.ts
 
-import type { DefaultConfigPalette } from '../../types/DefaultConfigPalette'
+import type { DefaultPaletteColors } from '../../types/DefaultPaletteColors'
 
-export default <DefaultConfigPalette>{
+export default <DefaultPaletteColors>{
     'default': 'text-gray-800 bg-gray-200 dark:bg-gray-800 dark:text-gray-200',
     error: 'text-rose-800 bg-rose-200 dark:bg-rose-800 dark:text-rose-200',
     primary: 'text-blue-800 bg-blue-200 dark:bg-blue-800 dark:text-blue-200',
