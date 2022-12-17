07cad2f10ab29cc36a111b2b90426d78954c1fa0
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-16 23:59:28 -0500

    replacing TextSizes prop type to new SizesText prop type in VvList.vue compo

--- src/components/lists/VvList.vue
+++ src/components/lists/VvList.vue
@@ -10,7 +10,7 @@
     import type { PropType } from 'vue'
     import type { DefaultPaletteColors } from '../../types/DefaultPaletteColors'
     import type { DefaultPalettes } from '../../types/DefaultPalettes'
-    import type { TextSizes } from '../../types/TextSizes'
+    import type { SizesText } from '../../types/SizesText'
 
     export default defineComponent({
 
@@ -30,7 +30,7 @@
                 default: VvConfig.defaults.VvList.markerPalette,
             },
             size: {
-                type: String as PropType<keyof TextSizes>,
+                type: String as PropType<keyof SizesText>,
                 default: VvConfig.defaults.VvList.size,
             },
             tag: {
