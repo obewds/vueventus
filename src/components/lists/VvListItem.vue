f72072da460c0bd4ff508d8d80d3c64afa88ba99
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-12-16 22:55:57 -0500

    adding vue PropType syntax to VvListItem prop types

--- src/components/lists/VvListItem.vue
+++ src/components/lists/VvListItem.vue
@@ -5,13 +5,17 @@
     import { computed, defineComponent, inject } from 'vue'
     import VvConfig from '../../configs/VvConfig.js'
 
+    import type { PropType } from 'vue'
+    import type { DefaultPaletteColors } from '../../types/DefaultPaletteColors'
+    import type { DefaultPalettes } from '../../types/DefaultPalettes'
+
     export default defineComponent({
 
         name: 'VvListItem',
 
         props: {
             color: {
-                type: String,
+                type: String as PropType<keyof DefaultPaletteColors>,
                 default: VvConfig.defaults.VvListItem.color,
             },
             enableColoredSymbols: {
@@ -19,15 +23,15 @@
                 default: VvConfig.defaults.VvListItem.enableColoredSymbols,
             },
             palette: {
-                type: String,
+                type: String as PropType<keyof DefaultPalettes>,
                 default: VvConfig.defaults.VvListItem.palette,
             },
             symbolColor: {
-                type: String,
+                type: String as PropType<keyof DefaultPaletteColors>,
                 default: VvConfig.defaults.VvListItem.symbolColor,
             },
             symbolPalette: {
-                type: String,
+                type: String as PropType<keyof DefaultPalettes>,
                 default: VvConfig.defaults.VvListItem.symbolPalette,
             },
         },
@@ -36,14 +40,16 @@
             
             const vv = Object.keys( inject( 'vv', {} ) ).length > 0 ? inject<typeof VvConfig>('vv') : VvConfig
 
-            let baseClasses = vv?.text.base()
-
             let classes = computed( () => {
 
-                let output = [baseClasses]
+                let output = []
 
-                if ( vv?.text?.palettes?.[props.palette as keyof typeof vv.text.palettes]?.[props.color] ) {
-                    output.push( vv.text.palettes[props.palette as keyof typeof vv.text.palettes][props.color] )
+                if ( vv?.text.base() ) {
+                    output.push( vv?.text.base() )
+                }
+
+                if ( vv?.text?.palettes?.[props.palette]?.[props.color] ) {
+                    output.push( vv.text.palettes[props.palette][props.color] )
                 }
 
                 return output.join(' ').trim()
@@ -52,10 +58,10 @@
 
             let symbolClasses = computed( () => {
 
-                let output: string[] = []
+                let output = []
 
-                if ( vv?.text?.palettes?.[props.symbolPalette as keyof typeof vv.text.palettes]?.[props.symbolColor] ) {
-                    output.push( vv.text.palettes[props.symbolPalette as keyof typeof vv.text.palettes][props.symbolColor] as string )
+                if ( vv?.text?.palettes?.[props.symbolPalette]?.[props.symbolColor] ) {
+                    output.push( vv.text.palettes[props.symbolPalette][props.symbolColor] )
                 }
 
                 return output.join(' ').trim()
