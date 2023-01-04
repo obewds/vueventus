9f9b6f52d89535d4052c84359a2496d8038094a2
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-01-04 11:44:42 -0500

    deleting now removed and depreciated ts interfaces that have all been replaced in lib

--- src/types/ButtonsSizes.ts
+++
@@ -1,9 +0,0 @@
-// ./src/types/ButtonsSizes.ts
-
-// BUG: This interface is DEPRECIATED
-// TODO: Remove this interface from the package
-// use the "SizesButtons" interface instead
-
-import type { SizesButtons } from './SizesButtons'
-
-export interface ButtonsSizes extends SizesButtons {}
