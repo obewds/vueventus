734d12fc639b1b7a634b7c8900c47f9cbb0fe890
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-01-04 19:35:45 -0500

    improving coverage of forceBodyClick.test.js

--- tests/helpers/forceBodyClick.test.js
+++ tests/helpers/forceBodyClick.test.js
@@ -1,10 +1,30 @@
 // ./tests/helpers/forceBodyClick.test.js
 
+import { mount } from '@vue/test-utils'
 import { forceBodyClick } from '../../src/index'
 
 
 test('forceBodyClick helper module returns a truthy value', async () => {
     
     expect(forceBodyClick).toBeTruthy()
+
+})
+
+
+test('forceBodyClick helper module handles a body click event', async () => {
+
+    const testButtonComponent = {
+        template: '<button @click="handleClick">Test Button</button>',
+        methods: {
+            handleClick() {
+                forceBodyClick()
+            }
+        }
+    }
+
+    const wrapper = mount(testButtonComponent)
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.html()).toContain('Test Button')
 
 })
