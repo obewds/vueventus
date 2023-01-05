d0f7109d4d1898ddc15b9829c186043c52c26d01
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-01-04 19:35:37 -0500

    improving coverage of appColorPairs.test.js

--- tests/helpers/appColorPairs.test.js
+++ tests/helpers/appColorPairs.test.js
@@ -1,10 +1,20 @@
 // ./tests/helpers/appColorPairs.test.js
 
 import { appColorPairs } from '../../src/index'
+import appColors from '../../src/app.colors.json'
 
 
 test('appColorPairs helper module returns a truthy value', async () => {
     
     expect(appColorPairs).toBeTruthy()
+
+})
+
+
+test('appColorPairs helper module returns expected values', async () => {
+
+    const result = appColorPairs(appColors)
+    
+    expect(result).toBeTruthy()
 
 })
