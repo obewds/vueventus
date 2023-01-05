49f2e178b8dc86e2355702c54fe77f508d29172e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-01-05 07:24:57 -0500

    adding new verifyRunTime.ts module

---
+++ src/helpers/verifyRunTime.ts
@@ -0,0 +1,27 @@
+// ./src/validation/verifyRunTime.ts
+
+import verifyType from './verifyType'
+
+export default function ( data: string, decimals: 1|2|3 = 3 ): boolean {
+
+    if ( verifyType(data, 'string') ) {
+
+        if ( decimals !== 1 && decimals !== 2 && decimals !== 3 ) {
+            return false
+        }
+
+        if ( decimals === 1 ) {
+            return /^([0-2]?[0-3][:])?([0-5]?[0-9][:])?([0-5]?[0-9])([.][0-9]{1})$/.test(data) ? true : false
+        }
+        else if ( decimals === 2 ) {
+            return /^([0-2]?[0-3][:])?([0-5]?[0-9][:])?([0-5]?[0-9])([.][0-9]{2})$/.test(data) ? true : false
+        }
+        else if ( decimals === 3 ) {
+            return /^([0-2]?[0-3][:])?([0-5]?[0-9][:])?([0-5]?[0-9])([.][0-9]{3})$/.test(data) ? true : false
+        }
+
+    }
+
+    return false
+
+}
