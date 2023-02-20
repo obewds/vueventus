56a48c702f39abbf675893a88b02c318924d53ac
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-02-20 18:47:28 -0500

    updating cli/helpers/moveNuxtTsFilesToRoot.mjs to cover remaining nuxi install files and remove nuxi directory after done

--- cli/helpers/moveNuxtTsFilesToRoot.mjs
+++ cli/helpers/moveNuxtTsFilesToRoot.mjs
@@ -5,20 +5,28 @@ import fs from 'fs-extra'
 import moveFile from './moveFile.mjs'
 import cwd from './cwd.mjs'
 
-export default function (viteDirectoryString) {
+export default function (nuxtDirString) {
 
     const files = [
         { from: '.gitignore', to: '.gitignore' },
         { from: 'app.vue', to: 'app.vue' },
         { from: 'README.md', to: 'README-NUXT.md' },
         { from: 'tsconfig.json', to: 'tsconfig.json' },
+        { from: 'public/favicon.ico', to: 'public/favicon.ico' },
+        { from: '.npmrc', to: '.npmrc' },
     ]
 
     files.forEach( (file) => {
-        moveFile(cwd + '/' + viteDirectoryString + '/' + file.from, cwd + '/' + file.to)
+        moveFile(cwd + '/' + nuxtDirString + '/' + file.from, cwd + '/' + file.to)
     })
 
-    fs.remove(cwd + '/' + viteDirectoryString + '/package.json', err => {
+    // fs.remove(cwd + '/' + nuxtDirString + '/package.json', err => {
+    //     if (err) return console.error(err)
+    //     // console.log('success!')
+    // })
+
+    // remove the nuxt generated directory as files have been moved out and up to root
+    fs.remove(cwd + '/' + nuxtDirString, err => {
         if (err) return console.error(err)
         // console.log('success!')
     })
