5995eb637041d9881f2b5346005cd20c468644cd
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-02-21 00:13:03 -0500

    integrating new nuxt variations of Vv Components into nuxt install cli script

--- cli/installers/addNuxtTs.mjs
+++ cli/installers/addNuxtTs.mjs
@@ -7,8 +7,22 @@ import appVueNuxtFile from '../generators/appVueNuxtFile.mjs'
 import appVvTsFile from '../generators/appVvTsFile.mjs'
 import pagesIndexNuxtFile from '../generators/pagesIndexNuxtFile.mjs'
 import nuxtConfigTsFile from '../generators/nuxtConfigTsFile.mjs'
-import vvAnchorVueFile from '../generators/vvAnchorVueFile.mjs'
+import tailwindConfigTsNuxtFile from '../generators/tailwindConfigTsNuxtFile.mjs'
 
+import vvAnchorVueFile from '../generators/vvAnchorVueFile.mjs'
+import vvButtonVueFile from '../generators/vvButtonVueFile.mjs'
+import vvCheckboxVueFile from '../generators/vvCheckboxVueFile.mjs'
+import vvColorModeButtonVueFile from '../generators/vvColorModeButtonVueFile.mjs'
+import vvElVueFile from '../generators/vvElVueFile.mjs'
+import vvFormGroupVueFile from '../generators/vvFormGroupVueFile.mjs'
+import vvInputVueFile from '../generators/vvInputVueFile.mjs'
+import vvListVueFile from '../generators/vvListVueFile.mjs'
+import vvListboxVueFile from '../generators/vvListboxVueFile.mjs'
+import vvListItemVueFile from '../generators/vvListItemVueFile.mjs'
+import vvQuadFormGroupVueFile from '../generators/vvQuadFormGroupVueFile.mjs'
+import vvRadioVueFile from '../generators/vvRadioVueFile.mjs'
+import vvSelectVueFile from '../generators/vvSelectVueFile.mjs'
+import vvTextareaVueFile from '../generators/vvTextareaVueFile.mjs'
 
 import cwd from '../helpers/cwd.mjs'
 import moveNuxtTsFilesToRoot from '../helpers/moveNuxtTsFilesToRoot.mjs'
@@ -25,9 +39,7 @@ export default function (userOpts, stackObj, installPkgsArr) {
     run(`npm pkg set scripts.preview="nuxt preview"`)
     run(`npm pkg set scripts.postinstall="nuxt prepare"`)
 
-    // copy each nuxt 3 generated file 
-    // from the nuxi generated directory 
-    // back up into the root directory
+    // copy each nuxt 3 generated file from the nuxi generated directory back up into the root directory
     moveNuxtTsFilesToRoot(userOpts.name)
 
     // generate the vueventus configured (with deps) nuxt.config.ts file
@@ -38,15 +50,26 @@ export default function (userOpts, stackObj, installPkgsArr) {
 
     installPkgsArr.push('nuxt@latest')
 
+
+
     // install tailwind css
     run(`npm install @nuxtjs/tailwindcss --save-dev`)
 
     installPkgsArr.push('@nuxtjs/tailwindcss')
 
+    // install tailwind.config.ts file
+    if ( userOpts.files.includes( stackObj.files.tailwindConfigTs.name ) ) {
+        fs.outputFileSync(cwd + stackObj.files.tailwindConfigTs.path + stackObj.files.tailwindConfigTs.name, tailwindConfigTsNuxtFile(), { flag: 'w+' })
+    }
+
+
+
     // install headless ui
     run(`npm install nuxt-headlessui @headlessui/tailwindcss --save-dev`)
 
     installPkgsArr.push('nuxt-headlessui', '@headlessui/tailwindcss')
+
+
 
     // install app.vv.ts file
     if ( userOpts.files.includes( stackObj.files.appVvTs.name ) ) {
@@ -68,7 +91,21 @@ export default function (userOpts, stackObj, installPkgsArr) {
     // copy the VueVentus starter end user app component files 
     // from the cli stubs file generators
     const composVv = cwd + '/components/Vv/'
+
     fs.outputFileSync(composVv + 'Anchor.vue', vvAnchorVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Button.vue', vvButtonVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Checkbox.vue', vvCheckboxVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'ColorModeButton.vue', vvColorModeButtonVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'El.vue', vvElVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'FormGroup.vue', vvFormGroupVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Input.vue', vvInputVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'List.vue', vvListVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Listbox.vue', vvListboxVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'ListItem.vue', vvListItemVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'QuadFormGroup.vue', vvQuadFormGroupVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Radio.vue', vvRadioVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Select.vue', vvSelectVueFile(true), { flag: 'w+' })
+    fs.outputFileSync(composVv + 'Textarea.vue', vvTextareaVueFile(true), { flag: 'w+' })
 
 
 
