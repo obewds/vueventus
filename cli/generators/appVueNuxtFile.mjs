ef2c7e08dc6d617fe0540efebbbdb5832baf6357
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-02-21 18:36:44 -0500

    adding VvColorModeButton to nuxt stack cli generator appVueNuxtFile.mjs

--- cli/generators/appVueNuxtFile.mjs
+++ cli/generators/appVueNuxtFile.mjs
@@ -4,16 +4,31 @@ export default function () {
 
 const output = `<!-- ./app.vue -->
 
+<script setup lang="ts">
+
+    import type { ValidColorModes } from '@obewds/vueventus'
+
+    const colorMode = typeof window !== 'undefined' && localStorage && localStorage.getItem('colorMode') ? localStorage.getItem('colorMode') as ValidColorModes : 'light' as ValidColorModes
+
+</script>
+
 <template>
 
     <div>
 
         <NuxtPage/>
 
+        <aside class="fixed bottom-0 right-0 text-right z-40">
+            <nav class="relative bottom-0 pb-3 pr-4">
+                <div class="flex flex-col justify-end space-y-3">
+                    <VvColorModeButton palette="solid" color="primary" :mode="colorMode"/>
+                </div>
+            </nav>
+        </aside>
+
     </div>
 
 </template>
-
 
 `
 
