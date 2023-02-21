16b3004d38f3094fbc3ef7721c41ad70af34d86b
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-02-20 19:13:26 -0500

    adding new cli/generators/pagesIndexNuxtFile.mjs

---
+++ cli/generators/pagesIndexNuxtFile.mjs
@@ -0,0 +1,27 @@
+// ./cli/generators/pagesIndexNuxtFile.mjs
+
+export default function () {
+
+    const output = `<!-- ./pages/index.vue -->
+
+<template>
+
+    <div>
+
+        <p>This is the <samp>./pages/index.vue</samp> page component!</p>
+
+        <VvAnchor>
+            VvAnchor Component
+        </VvAnchor>
+
+    </div>
+
+</template>
+
+
+`
+
+return output
+    
+}
+    
