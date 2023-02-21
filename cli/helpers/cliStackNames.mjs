453b2dfac88aa2919fbc34b9705ae67622230c0d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-02-21 04:33:17 -0500

    updating nuxt stack name to follow conventions of other stack options

--- cli/helpers/cliStackNames.mjs
+++ cli/helpers/cliStackNames.mjs
@@ -4,6 +4,6 @@ export default {
     vueTwViteTs: 'Vue 3, Tailwind CSS, Headless UI, Vue Router, Vite & Typescript',
     vueTwViteSsgMdTs: 'Vite-SSG, Vue 3, Tailwind CSS, Headless UI, Pinia, Markdown, Auto-Loading & Typescript',
     // vueTwViteFirebaseTs: 'Vue 3, Tailwind CSS, Vue Router, Firebase, Vite & Typescript',
-    vueTwNuxt3: 'Vue 3, Nuxt 3, Nuxt Content, Tailwind CSS, Headless UI, Pinia, & Typescript',
+    vueTwNuxt3: 'Nuxt 3, Vue 3, Tailwind CSS, Headless UI, Pinia, Nuxt Content, & Typescript',
     // vueTwViteLaravelTs: 'Vue 3, Tailwind CSS, Laravel, Vite & Typescript',
 }
