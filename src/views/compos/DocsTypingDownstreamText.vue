a444c51b48b33ebd629d4f954eedc0dfdfcd202d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-04-17 18:57:24 -0400

    adding new DocsTypingDownstreamText.vue compo to make docs code DRYer

---
+++ src/views/compos/DocsTypingDownstreamText.vue
@@ -0,0 +1,21 @@
+<!-- ./src/views/compos/DocsTypingDownstreamText.vue -->
+
+<script setup lang="ts">
+
+    const props = defineProps({
+        name: {
+            type: String,
+            default: '$frontmatter.title',
+        },
+    })
+
+</script>
+
+
+<template>
+
+    <div class="w-full mt-3">
+        If you are building a component that uses a nested {{ name }} and want to pass through strict typing into your code base when using your built component, you can add typing for Vue props like this:
+    </div>
+
+</template>
