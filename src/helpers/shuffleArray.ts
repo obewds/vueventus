7b93d8e4c2c2c950c1de38e31e25682087e98a72
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-05-09 07:40:09 -0400

    adding new shuffleArray helper module

---
+++ src/helpers/shuffleArray.ts
@@ -0,0 +1,21 @@
+// ./src/helpers/shuffleArray.ts
+
+export default function<Type>(array: Type[]): Type[] {
+
+    let newArray = [...array]
+    
+    for (let i = newArray.length - 1; i > 0; i--) {
+
+        // get a random index
+        let j = Math.floor(Math.random() * (i + 1))
+
+        // swap elements
+        let t = newArray[i]
+        newArray[i] = newArray[j]
+        newArray[j] = t
+
+    }
+
+    return newArray
+
+}
